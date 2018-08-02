4ceef98825dd32cca1ccffa4f07a2abc6e5562fb
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-02 14:10:52 -0700

    saved sqlite3 in data file

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/sqlite3'
     }
   },
-  
+
 
   // staging: {
   //   client: 'postgresql',
