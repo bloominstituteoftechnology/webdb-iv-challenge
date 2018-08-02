8ee987a1bb40fd567e196445df5f0aa0f7215e11
cmok4290 <cmok4290@gmail.com>
2018-08-02 16:35:06 -0400

    added migrations

---
+++ migrations/20180802160921_recipes_ingredients.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipes_ingredients', tbl => {
+    tbl.increments();
+    tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes');
+    tbl.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients');
+    tbl.integer('quantity')
+        .unsigned()
+        .notNullable();
+    tbl.text('directions');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('recipes_ingredients');
+};
