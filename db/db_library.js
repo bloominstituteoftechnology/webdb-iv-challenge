df14a6be512121cf0fe21b77d7b2a5166ae780ab
Erin Meredith <ekmeredith@att.net>
2018-08-02 18:23:25 -0400

    getDishes and getDish by ID functioning

---
+++ db/db_library.js
@@ -0,0 +1,63 @@
+const db = require('./db');
+
+module.exports = {
+    getDishes: function() {
+        return db('dishes as d').select('d.dish');
+    },
+
+    getDish: function(id) {
+        let query = db('dishes as d');
+        if(id) {
+            query.select('d.dish')
+            .where('d.id', id)
+        
+       const promises = [query, this.getRelatedRecipes(id)]
+
+        return Promise.all(promises).then(function(results) {
+            let [dishes, recipes] = results;
+            let dish = dishes[0];
+            dish.recipes = recipes.map(r => r.recipe_name);
+    
+            return dish;
+        })}
+    },
+
+    getRelatedRecipes: function(id) {
+        return db('recipes as r')
+        .join('dishes as d', 'd.id', 'r.dishId')
+        .select('d.dish', 'r.recipe_name')
+        .where('r.dishId', id)
+    },
+
+    addDish: function(dish) {
+
+    },
+
+    getRecipes: function() {
+        let query = db('recipes as r');
+        let id = query.select('r.id');
+        return  db('recipes as r')
+        .join('dishes as d', 'r.dishId', 'd.id')
+        .select('r.recipe_name', 'd.dish')
+        .where('d.id', 'r.dishId')
+
+    },
+
+    getRecipe: function(id) {
+        let query = db('recipes as r');
+        if(id) {
+            query.select('r.recipe_name')
+            .where('r.id', id);
+        };
+        return query.join('dishes as d', 'r.dishId', 'd.id')
+        .select('r.recipe_name', 'd.dish')
+        .where('d.id', id)
+      
+    },
+
+    addRecipe: function() {
+
+    }
+
+}
+
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const server = express();
-const db = require('./db/db.js');
+const db = require('./db/db_library.js');
 
 server.use(express.json());
 
 
 server.get('/api/dishes', (req, res) => {
-    db('dishes')
+    db.getDishes()
     .then(response => {
         res.status(200).json(response)
     })
@@ -14,6 +14,40 @@ server.get('/api/dishes', (req, res) => {
         res.status(500).json({error: 'The dishes could not be retrieved.'})
     })
 })
+
+server.get('/api/dishes/:id', (req, res) => {
+    const id = req.params.id;
+    db.getDish(id)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'The dish could not be retrieved.'})
+    })
+})
+
+server.get('/api/recipes', (req, res) => {
+    db.getRecipes()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'The recipes could not be retrieved.'})
+    })
+})
+
+server.get('/api/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    db.getRecipe(id)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'The recipe could not be retrieved.'})
+    })
+})
+
+
 
 const port = 8000;
 server.listen(port, () => console.log('API running'));
