6eccca428994f41d66f2b748ad6f420df0fe3782
DirupT <iamcooled@gmail.com>
2018-08-02 15:30:12 -0500

    updated helper function names

--- api/dishes/dishRoutes.js
+++ api/dishes/dishRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        const response = await dishes.get();
+        const response = await dishes.getDishes();
         return res.status(200).json(response);
     } catch (err) {
         return next({ code: 500, error: 'The dishes information could not be retrieved.' });
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
     try {
-        const response = await dishes.get(req.params.id);
+        const response = await dishes.getDish(req.params.id);
         return res.status(200).json(response);
     } catch (err) {
         return next({ code: 500, error: 'The dish information could not be retrieved.' });
--- data/helpers/dishesDb.js
+++ data/helpers/dishesDb.js
@@ -1,25 +1,22 @@
 const db = require('../db');
 
 module.exports = {
-    get: id => {
-        let query = db('dishes');
+    getDishes: () => {
+        return db('dishes');
+    },
+    getDish: id => {
+        let query = db('dishes').where('id', id).first();
 
-        if (id) {
-            query.where('id', id).first();
+        const promises = [query, db('recipes').where('dish_id', id)];
 
-            const promises = [query, db('recipes').where('dish_id', id)];
+        return Promise.all(promises).then(results => {
+            if (results[1].length > 0) {
+                let [dish, recipes] = results;
+                dish.recipes = recipes.map(r => r.name);
 
-            return Promise.all(promises).then(results => {
-                if (results[1].length > 0) {
-                    let [dish, recipes] = results;
-                    dish.recipes = recipes.map(r => r.name);
-
-                    return dish;
-                }
-                return results[0];
-            })
-        }
-
-        return query;
+                return dish;
+            }
+            return results[0];
+        })
     }
 }
