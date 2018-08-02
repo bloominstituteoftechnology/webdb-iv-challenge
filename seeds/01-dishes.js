75eb82f1d2b4b2a6db3bfc5617131fed460d49e6
Kevin <brackkevin@gmail.com>
2018-08-02 16:36:41 -0500

    Add recipe table migration and seed

--- seeds/01-dishes.js
+++ seeds/01-dishes.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("dishes").insert([
-        { name: "taco" },
+        { name: "chicken" },
         { name: "burger" },
         { name: "steak" }
       ]);
--- seeds/02-recipes.js
+++ seeds/02-recipes.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("recipes")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("recipes").insert([
+        { name: "Filet", dish_id: "3" },
+        { name: "Prime Rib", dish_id: "3" },
+        { name: "Bacon Cheeseburger", dish_id: "2" }
       ]);
     });
 };
