241a7e3c816496e0b9e41f68e736e10c4b8c1343
DirupT <iamcooled@gmail.com>
2018-08-02 15:26:03 -0500

    updated helper to get correct recipes

--- api/dishes/dishRoutes.js
+++ api/dishes/dishRoutes.js
@@ -3,12 +3,21 @@ const dishes = require('../../data/helpers/dishesDb');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
         const response = await dishes.get();
         return res.status(200).json(response);
     } catch (err) {
-        return res.status(500).json(err);
+        return next({ code: 500, error: 'The dishes information could not be retrieved.' });
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const response = await dishes.get(req.params.id);
+        return res.status(200).json(response);
+    } catch (err) {
+        return next({ code: 500, error: 'The dish information could not be retrieved.' });
     }
 })
 
--- data/helpers/dishesDb.js
+++ data/helpers/dishesDb.js
@@ -2,14 +2,24 @@ const db = require('../db');
 
 module.exports = {
     get: id => {
-        let query = db('dishes as d');
+        let query = db('dishes');
 
         if (id) {
-            query
-                .join('recipes as r', 'r.dish_id', 'd.id')
-                .select('d.name as Dish', 'r.name as Recipes')
-                .where('d.id', id);
+            query.where('id', id).first();
+
+            const promises = [query, db('recipes').where('dish_id', id)];
+
+            return Promise.all(promises).then(results => {
+                if (results[1].length > 0) {
+                    let [dish, recipes] = results;
+                    dish.recipes = recipes.map(r => r.name);
+
+                    return dish;
+                }
+                return results[0];
+            })
         }
+
         return query;
     }
 }
