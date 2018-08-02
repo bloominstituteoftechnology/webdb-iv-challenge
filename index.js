b14b1631d734c7783d2ff8a10ac777347d0a3b10
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-02 17:27:50 -0400

    Created more helper functions

--- index.js
+++ index.js
@@ -24,6 +24,49 @@ server.get('/dishes', (req, res) => {
       .catch(err => res.status(500).json(err))
 })
 
+
+//Get Dish by ID
+server.get('/dishes/:id', (req, res) => {
+  dishesDb
+  .getDish(req.params.id)
+  .then(dish => {
+    if (!dish) {
+      res
+      .status(404)
+      .json({ message: "The dish with the specified ID does not exist." });
+  }
+  res
+  .status(200)
+  .json(dish);
+})
+.catch(err => {
+      res
+      .status(500)
+      .json({ error: "The user information could not be retrieved."});
+  });
+})
+
+//Add Dish
+server.post('/dishes', (req, res) => {
+  const {name} = req.body;
+  if (!name) {
+    res.status(400).json({errorMessage: "Please provide name for the dish."})
+    return;
+}
+dishesDb
+.addDish({name})
+.then(response => {
+  res.status(201).json({name})
+})
+.catch(error => {
+  res.status(500).json({error: "There was an error while saving the dish to the database" })
+})
+})
+
+
+
+
+
 const port = 3300;
 server.listen(port, function() {
  console.log(`\n=== Yo, Web API Listening on http://localhost:${port} ===\n`);
