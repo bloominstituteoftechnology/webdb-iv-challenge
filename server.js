b10377383d6f9aa32194504325890c89d8135990
Sam Khaled <samkhaled@outlook.com>
2018-08-03 04:23:04 -0400

    modified server file to manipulate data as per MVP

--- server.js
+++ server.js
@@ -18,8 +18,55 @@ server.get('/', (req, res) => {
   res.send('Hello World <br><h1>RDBMS-RECIPE-BOOK : working on mvp</h1> <h3>Sam Khaled</h3>');
 });
 
+server.get('/dishes', (req, res) => {
+	knex('dishes')
+		.then(dishes => {
+			res.status(200).json(dishes);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		})
+});
 
+ server.get('/dishes/:id', (req, res) => {
+	const { id } = req.params;
+ 	knex('dishes').where({ id })
+		.then(dishes => {
+			if (dishes.length > 0) {
+				res.status(200).json(dishes);
+			} else {
+				res.status(404).json({message: 'dishes does not exist'});
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		})
+});
 
+server.get('/recipe', (req, res) => {
+	knex('recipe')
+		.then(recipe => {
+			res.status(200).json(recipe);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		})
+});
+
+ server.get('/recipe/:id', (req, res) => {
+	const { id } = req.params;
+ 	knex('recipe').where({ id })
+		.then(recipe => {
+			if (recipe.length > 0) {
+				res.status(200).json(recipe);
+			} else {
+				res.status(404).json({message: 'recipe does not exist'});
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		})
+});
 
 
 // server.listen(port, () => console.log(`\n Server is running on http://localhost:${port} === \n`));
