de284c524dd333e605765c192d894e839677abb7
JJ Ashcraft <jj@jjashcraft.com>
2018-08-03 08:56:49 -0400

    dishes and ingredients  initial table migration created

--- migrations/20180803080214_dishes_table.js
+++ migrations/20180803080214_dishes_table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
   //add dishes_table
+  return knex.schema.createTable('dishes', function(tbl) {
+    //pk
+    tbl.increments();
+    tbl.string('dish_name', 255)
+    .unique()
+    .notNullable();
+    //.defaultTo('whatever you want to default')
+
+  })
 };
 
 exports.down = function(knex, Promise) {
   //remove_dishes_table
+  return knex.schema.dropTableIfExists('dishes');
 };
