28ba33f78225918826229013e48259c32cb0be35
Daniel Chamorro <dan@dcitllc.com>
2018-08-08 13:23:14 -0400

    MVP completed - Using joins to diplay data in route from multiple tables using getRecipe(id)

--- data/helpers.js
+++ data/helpers.js
@@ -1,10 +1,18 @@
 const db = require("./db");
 
+// module.exports = {
+//   getRecipe: function(recipeId) {
+//     return db("dishes")
+//       .select()
+//       .where({ id: recipeId });
+//   }
+// };
+
 module.exports = {
   getRecipe: function(recipeId) {
     return db("recipes")
       .join("dishes", { "dishes.id": "recipes.dish_id" })
-      .select("dishes.id", "recipes.recipe_name")
+      .select("recipes.recipe_name", "dishes.dish_name")
       .where({ "recipes.id": recipeId });
   }
 };
