15c782bc356f8a6b12345c5dc13c64c887fb88d2
Daniel Chamorro <dan@dcitllc.com>
2018-08-08 10:36:40 -0400

    Added recipes seed data to recipes table

--- seeds/002_recipes.js
+++ seeds/002_recipes.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("recipes")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("recipes").insert([
+        { recipe_name: "Grandma Recipe", dish_id: 1, ingredient_id: 1 },
+        { recipe_name: "Father Recipe", dish_id: 2, ingredient_id: 1 },
+        { recipe_name: "Mother Recipe", dish_id: 1, ingredient_id: 2 }
       ]);
     });
 };
