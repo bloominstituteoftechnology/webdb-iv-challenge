baeb2f927a4bcbc4f7527011e6e240dcfcca12dd
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-08-09 15:38:55 -0600

    ingredients table

---
+++ migrations/20180809153321_ingredients_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('ingredients', ingredient => {
+      ingredient.increments()
+
+      ingredient
+      .string('name')
+      .notNullable()
+      .unique()
+  })
+};
+
+exports.down = function(knex, Promise) {
+  
+};
