af3111f8db7924e00664d587f2477ef3bda9ba97
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-08-09 16:38:46 -0600

    create seed

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const db = require('./data/db');
+
 const server = express();
 server.use(express.json());
 
 const PORT = 3300;
+
+server.get('/dishes', (req, res) => {
+    db('dishes')
+    .then(dish => {
+        res.status(200).json(dish)
+    })
+    .catch(() => {
+        res.status(500).json({message: "error"})
+    })
+});
 
 
 
