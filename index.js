4afda9628601189d81a2da577e8f7030c19a7ab2
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-09 19:26:42 -0700

    Create migrations for ingedients_table and ingredients recipes table Create seeds for dishes, recipes, seeds, and recipes Implement GET dishes endpoint

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ server.use(express.json())
 
 const PORT = 3300
 
+server.get('/dishes', (req,res) => {
+  db('dishes')
+  .then(dish => {
+    res.status(200).json(dish)
+  })
+  .catch((err) => {
+    res.status(500).json(err)
+  })
+})
 
 server.listen(PORT, () => {
   console.log(`Server up on ${PORT}`)
--- migrations/20180809134105_recipe_table.js
+++ migrations/20180809134105_recipe_table.js
@@ -1,6 +1,22 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipes', recipe => {
+    recipe.increments()
+
+    recipe
+      .string('name')
+      .notNullable()
+      .unique()
+
+      recipe
+        .integer('dish_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      })
 };
 
 exports.down = function(knex, Promise) {
