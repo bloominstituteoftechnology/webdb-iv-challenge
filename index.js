bf6e28d27cf2a41865ec90e82e30e7c9887a9444
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-09 23:14:42 -0700

    Implement CRUD functionality for dishes and recipes

--- index.js
+++ index.js
@@ -18,6 +18,108 @@ server.get('/dishes', (req,res) => {
   })
 })
 
+server.get('/dishes/:id', (req, res) => {
+  const id = req.params.id;
+  db('dishes').where('id', id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(response => {
+      res.status(500).json(err);
+    });
+});
+
+server.post('/dishes', (req, res) => {
+  const dish = req.body
+  db.insert(dish).into('dishes')
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(response => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete('/users/:id', (req, res) => {
+  const id = req.body.id;
+  db('users').where('id', id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(response => {
+      res.status(500).json(err);
+    });
+});
+
+server.put('/dishes/:id', (req, res) => {
+  const id = req.params.id;
+  const recipe = req.params.id;
+  db('users').where('id', id)
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+});
+ 
+//=========Recipes==========
+
+server.get('/recipes', (req, res) => {
+  db('recipes')
+  .then(response => {
+    res.status(200).json(response);
+  })
+  .catch(response => {
+    res.status(500).json(err);
+  });
+});
+
+server.get('/recipes:id', (req, res) => {
+  const id = req.body.id;
+  db('recipes').where('id', id)
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+});
+
+server.post('/recipes', (req, res) => {
+  const recipe = req.body
+  db.insert(dish).into('recipes')
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+});
+
+server.delete('/recipes/:id', (req, res) => {
+  const id = req.params.id;
+  db('users').where('id', id)
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+});
+
+server.put('/recipes/:id', (req, res) => {
+  const id = req.params.id;
+  const recipe = req.params.id;
+  db('users').where('id', id)
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`Server up on ${PORT}`)
 })
--- seeds/004_recipes.js
+++ seeds/004_recipes.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('recipes').del()
+  return knex('ingredients_recipes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('recipes').insert([
+      return knex('ingredients_recipes').insert([
         {recipe_id: 1, ingredients_id: 1},
         {recipe_id: 2, ingredients_id: 1},
         {recipe_id: 3, ingredients_id: 1}
