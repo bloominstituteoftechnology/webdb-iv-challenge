2d3763da0fe6524d50eaac052c7837a44735fbf1
Sean <sean47900@gmail.com>
2018-09-06 17:20:22 -0500

    trying to implement GET

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const server = express();
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 
-server.get("/")
+server.get("/dishes", async (req,res)=>{  
+     try { 
+    const dishes = await db('dishes');
+    res.status(200).json(dishes);
+}
+catch (err) {
+    res.status(500).json(err);
+}});
 
 
 server.listen( 2000, () => console.log('===Server running port 2000==='))
