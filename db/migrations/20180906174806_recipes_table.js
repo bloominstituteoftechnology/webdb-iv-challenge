a1c7d5d3117b7e910bdf6cc1139d198d78a5e5da
Erik Kimsey <erik@erikkimsey.com>
2018-09-06 18:14:30 -0400

    Created migration for recipe

---
+++ db/migrations/20180906174806_recipes_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipes', function (tbl) {
+        tbl.increments();
+        tbl
+            .string('name', 128)
+            .notNullable()
+            .unique('name');
+        tbl
+            .integer('dish_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('dishes');
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('recipes');
+};
