ce6ab86fdd0a6c4601b6248eeda19e1223e9e0cf
Ashwin <aswinjeganath@gmail.com>
2018-09-07 02:51:12 +0530

    Set up seeds for dishes

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3',
+      filename: './db/recipe_book.sqlite3',
     },
     useNullAsDefault: true,
     migrations: {
