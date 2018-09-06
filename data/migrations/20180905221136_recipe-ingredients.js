2e4e340304ae6a6f61377ec7519dd0a9d2bcd181
Lauren <llwor94@gmail.com>
2018-09-05 22:20:58 -0700

    recipe ingredients schema set up

---
+++ data/migrations/20180905221136_recipe-ingredients.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipe_ingredients', table => {
+    table
+      .integer('recipe_id')
+      .notNullable()
+      .references('id')
+      .inTable('recipes');
+    table
+      .integer('ingredient_id')
+      .notNullable()
+      .references('id')
+      .inTable('ingredients');
+    table.integer('amount').notNullable();
+  });
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('recipe_ingredients');
+};
