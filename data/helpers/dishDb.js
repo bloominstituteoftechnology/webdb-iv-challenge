0c926e9879053330c0fe5ff4e5dceec58f1afbd7
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-06 17:09:36 -0400

    Added dish helper -get dishes -get a dish -add a dish -going to setup a basic server to test before moving on

---
+++ data/helpers/dishDb.js
@@ -0,0 +1,15 @@
+const db = require('../dbConfig.js');
+
+module.exports = {
+  get: function(id) {
+    let query = db('dishes');
+    if(id){
+      query.where('id', Number(id)).first();
+    }
+
+    return query;
+  }, //get's all dishes, or dish by id if provided
+  insert: function(dish) {
+    return db('dishes').insert(dish).then(ids => ({ id: ids[0] }));
+  },  
+};
