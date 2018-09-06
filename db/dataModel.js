2721159f5576fe76327651ea50cd9df3202b59fd
Grant Reighard <grant.reighard@gmail.com>
2018-09-06 18:28:19 -0400

    Got all endpoints up and working for recipes and dishes.

---
+++ db/dataModel.js
@@ -0,0 +1,85 @@
+const db = require('./dbConfig');
+
+module.exports = {
+    getDishes: function(id) {
+        let query = db('dishes');
+
+        if (id) {
+            return query
+                .where('id', id)
+                .first()
+                .then(dish => dish);
+        }
+
+        return query.then(dishes => {
+            return dishes;
+        });
+    },
+
+    addDish: function(dish) {
+        return db('dishes')
+            .insert(dish)
+            .then(([id]) => this.getDishes(id))
+    },
+
+    getDish: function(id) {
+        return query
+        .where('id', id)
+        .first()
+        .then(dish => dish);
+    },
+
+    updateDish: function(id, changes) {
+        return db('dishes')
+        .where('id', id)
+        .update(changes)
+        .then(count => (count > 0 ? this.getDishes(id) : null))
+    },
+
+    deleteDish: function(id) {
+        return db('dishes')
+        .where('id', id)
+        .del();
+    },
+
+    getRecipes: function(id) {
+        let query = db('recipes');
+
+        if (id) {
+            return query
+                .where('id', id)
+                .first()
+                .then(dish => dish);
+        }
+
+        return query.then(recipes => {
+            return recipes;
+        });
+    },
+
+    getRecipe: function(id) {
+        return query
+            .where('id', id)
+            .first()
+            .then(recipe => recipe);
+    },
+
+    addRecipe: function(recipe) {
+        return db('recipes')
+            .insert(recipe)
+            .then(([id]) => this.getRecipes(id))
+    },
+
+    updateRecipe: function(id, changes) {
+        return db('recipes')
+        .where('id', id)
+        .update(changes)
+        .then(count => (count > 0 ? this.getRecipes(id) : null))
+    },
+
+    deleteRecipe: function(id) {
+        return db('recipes')
+        .where('id', id)
+        .del();
+    },
+}
---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+// const dbConfig = require('./knexfile');
+// const db = knex(dbConfig.development);
+const db = require('./db/dataModel');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+// dishes endpoints
+server.post('/api/dishes', (req, res) => {
+    const { name } = req.body;
+
+    db.addDish({name})
+    .then(ids => {
+        res.status(201).json(ids)
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+server.get('/api/dishes', async (req, res) => {
+    try {
+        const response = await db.getDishes();
+        res.status(200).json(response);
+    } catch (ex) {
+        res.status(500).json(ex);
+    }
+})
+
+server.get('/api/dishes/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await db.getDishes(id);
+        res.status(200).json(response);
+    } catch (ex) {
+        res.status(500).json(ex);
+    }
+})
+
+server.put('/api/dishes/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    db.updateDish(id, {name})
+    .then(count => {
+        res.status(200).json(count);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+server.delete('/api/dishes/:id', (req, res) => {
+    const { id } = req.params;
+    db.deleteDish(id)
+    .then(count => {
+        res.status(200).json(count);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+
+// recipes endpoints
+
+server.post('/api/recipes', (req, res) => {
+    const { name, dish_id, ingredients, instructions } = req.body;
+
+    db.addRecipe({name, dish_id, ingredients, instructions})
+    .then(ids => {
+        res.status(201).json(ids)
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+server.get('/api/recipes', async (req, res) => {
+    try {
+        const response = await db.getRecipes();
+        res.status(200).json(response);
+    } catch (ex) {
+        res.status(500).json(ex);
+    }
+})
+
+server.get('/api/recipes/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await db.getRecipes(id);
+        res.status(200).json(response);
+    } catch (ex) {
+        res.status(500).json(ex);
+    }
+})
+
+server.put('/api/recipes/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, dish_id, ingredients, instructions } = req.body;
+    db.updateRecipe(id, {name, dish_id, ingredients, instructions})
+    .then(count => {
+        res.status(200).json(count);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+server.delete('/api/recipes/:id', (req, res) => {
+    const { id } = req.params;
+    db.deleteRecipe(id)
+    .then(count => {
+        res.status(200).json(count);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+
+server.listen(5000, console.log('\n=== Server listening on port 5000 ===\n'));
