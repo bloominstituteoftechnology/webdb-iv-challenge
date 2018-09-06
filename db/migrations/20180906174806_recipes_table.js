c2736136172ae847fcd2e62133ff3347bccb69eb
Erik Kimsey <erik@erikkimsey.com>
2018-09-06 19:09:46 -0400

    Created seed files for dishes, recipes, ingredients

--- db/migrations/20180906174806_recipes_table.js
+++ db/migrations/20180906174806_recipes_table.js
@@ -2,16 +2,23 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('recipes', function (tbl) {
         tbl.increments();
+        tbl.timestamp('created_at').defaultTo(knex.fn.now());
         tbl
             .string('name', 128)
             .notNullable()
-            .unique('name');
+            .unique('recipe_name');
         tbl
             .integer('dish_id')
             .unsigned()
             .notNullable()
             .references('id')
             .inTable('dishes');
+        tbl
+            .integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('ingredients');
     })
 };
 
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 const cors = require('cors');
 const helmet = require('helmet');
-const morgan = require('morgan');
-// const cors = require('');
-const port = 8080;
+const morgan = require('morgan');   
+const port = 8080;  
+// https://github.com/LambdaSchool/db-recipe-book/pull/73
 
 app.use(express.json());
 app.use(cors());
