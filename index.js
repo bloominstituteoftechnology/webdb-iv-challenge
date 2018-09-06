5a3793f7cec387d0b6bc0d3b675ef9e63dd84232
francistse23 <wtse.ucsd@gmail.com>
2018-09-06 14:26:11 -0700

    GET method for dishes implemented.

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const server = express();
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 
-server.get("/")
+server.get("/dishes", async (req, res) => {
+    try { 
+        const dishes = await db('dishes');
+        res.status(200).json(dishes);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
 server.listen( 8000, () => console.log('===Server running port 8000==='))
