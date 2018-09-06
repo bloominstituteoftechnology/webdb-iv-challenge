5f8debcf8d0040c2c029b548b22d09f1a6c28304
Carey <careycbaldwin@gmail.com>
2018-09-06 16:30:40 -0400

    added first migration - 'dishes' table

--- db/migrations/20180906162003_recipes_table.js
+++ db/migrations/20180906162003_recipes_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('dishes', function(tbl) {
+      tbl.increments();
+
+      tbl
+        .string('title', 128)
+        .notNullable()
+        .unique('title');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('dishes');
 };
