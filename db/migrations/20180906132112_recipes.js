8217dc477ba9d41e2205661910cc0d0ce7adc5fe
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-06 14:07:59 -0700

    Created tables (dishes, recipes, ingredients.)

---
+++ db/migrations/20180906132112_recipes.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipes', function(tbl) {
+        tbl.increments();
+        tbl
+          .string('dish_style')
+          .notNullable()
+          .unique('dish_style');
+        tbl
+          .string('dish_recipe')
+          .notNullable();
+        tbl
+          .integer('dishes_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('dishes');
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('recipes');
+  };
---
+++ db/seeds/01-dishes.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('dishes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('dishes').insert([
+        {dish_name: 'Tacos'},
+        {dish_name: 'Scrambled eggs'},
+        {dish_name: 'Chicken'}
+      ]);
+    });
+};
