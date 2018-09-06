9ed09c763f0d744accff7a3c1889767d6c650504
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-06 18:15:19 -0400

    Fixed get recipe by id -returns the id and name of a recipe along with an array of objects containing a recipe id and name

--- data/helpers/recipeDb.js
+++ data/helpers/recipeDb.js
@@ -9,9 +9,10 @@ module.exports = {
 
     return query;
   }, //get all recipes, or a recipe if id provided
+    //needs to be refactored to return other stuff
   insert: function(recipe) {
     return db('recipes')
             .insert(recipe)
             .then(ids => ({ id: ids[0] }));
-  },
+  }, //adds a recipe
 }
