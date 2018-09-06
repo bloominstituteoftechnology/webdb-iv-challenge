04f6ff05613ddb3388cd6eef2637c5c8643a4ce8
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-09-06 12:47:17 -0700

    implement getDish method to get single dish + recipes

--- data/helper.js
+++ data/helper.js
@@ -4,6 +4,11 @@ function getDishes(){
 	return db('dishes')
 }
 
+function getDish(id){
+	return db('dishes').join('recipes', 'recipes.dish_id', 'dishes.id').where({'dishes.id': id})
+}
+
 module.exports = {
-	getDishes
+	getDishes,
+	getDish
 }
--- index.js
+++ index.js
@@ -15,6 +15,16 @@ app.get('/dishes', async (req, res) => {
 	}
 });
 
+app.get('/dishes/:id', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const dish = await helper.getDish(id)
+		res.status(200).json(dish)
+	} catch(err) {
+		res.status(500).json(err);
+	}
+});
+
 app.listen(9000, () => {
   console.log('Server listening on 9000');
 });
