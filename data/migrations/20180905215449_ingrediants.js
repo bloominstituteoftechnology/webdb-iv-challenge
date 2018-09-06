7a257f2541b3e1e06c5bccc577a76ef097a1eb3c
Lauren <llwor94@gmail.com>
2018-09-05 22:10:15 -0700

    ingredients migration created

--- data/migrations/20180905215449_ingrediants.js
+++ data/migrations/20180905215449_ingrediants.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('ingredients', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.integer('price').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('ingredients');
 };
