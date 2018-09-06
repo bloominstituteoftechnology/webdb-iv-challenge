5a8cff66a03f73230d2d7973f7e1000ce05f3fcf
adamcglee <adamcglee@gmail.com>
2018-09-06 15:44:29 -0700

    updated migrations and seeds to provide proper data

--- db/seeds/01-recipes.js
+++ db/seeds/01-recipes.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('recipes_ing').truncate()
+  return knex('recipes').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('recipes_ing').insert([
-        {amount: 1, measurement: 'cup'},
-        {amount: 2, measurement: 'teaspoon'},
-        {amount: 3, measurement: 'tablespoon'}
+      return knex('recipes').insert([
+        {name: 'Crab Rangoon'},
+        {name: 'Orange Chicken'},
+        {name: 'Sweet and Sour Pork'},
+        {name: 'California Roll'},
+        {name: 'Chicken Teriyaki'},
+        {name: 'Gyoza'},
+        {name: 'Pancakes'},
+        {name: 'Breakfast Platter'},
+        {name: 'Belgian Waffle'},
       ]);
     });
 };
--- db/seeds/01-recipes_ing.js
+++ db/seeds/01-recipes_ing.js
@@ -1,19 +1,12 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('recipes').truncate()
+  return knex('recipes_ing').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('recipes').insert([
-        {name: 'Crab Rangoon'},
-        {name: 'Orange Chicken'},
-        {name: 'Sweet and Sour Pork'},
-        {name: 'California Roll'},
-        {name: 'Chicken Teriyaki'},
-        {name: 'Gyoza'},
-        {name: 'Pancakes'},
-        {name: 'Breakfast Platter'},
-        {name: 'Belgian Waffle'},
+      return knex('recipes_ing').insert([
+        {recipe_id: '4', ingredient_id: '1', amount: 1, measurement: 'cup'},
+        {recipe_id: '8', ingredient_id: '4', amount: 2, measurement: ''},
+        {recipe_id: '1', ingredient_id: '6', amount: 3, measurement: 'tablespoon'}
       ]);
     });
 };
