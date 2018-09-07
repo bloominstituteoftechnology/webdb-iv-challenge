dc9fb7461c1428fb824a2f167ee8ab3fe4626a67
codejoncode <jonathanjholloway@gmail.com>
2018-09-06 20:24:09 -0500

    added recipe_ingredients for the first recipe which is connected to the first dish

--- db/seeds/recipe_ingredients.js
+++ db/seeds/recipe_ingredients.js
@@ -5,9 +5,20 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('recipe_ingredients').insert([
-        {colName: 'rowValue1'},
-        {colName: 'rowValue2'},
-        {colName: 'rowValue3'}
+        {recipe_id: 1, ingredient_id: 1 , quantity: 2.5 , unit_of_measure: "cups"},
+        {recipe_id: 1, ingredient_id: 2, quantity: 2 , unit_of_measure:"cups"},
+        {recipe_id: 1, ingredient_id: 3 , quantity:1 , unit_of_measure:"tablespoon"},
+        {recipe_id: 1, ingredient_id: 4, quantity: 1, unit_of_measure:"teaspoon"},
+        {recipe_id: 1, ingredient_id:5 , quantity: .5, unit_of_measure:"teaspoon"},
+        {recipe_id: 1, ingredient_id:6 , quantity:2 , unit_of_measure:"lightly beaten"},
+        {recipe_id: 1, ingredient_id:7 , quantity:1 , unit_of_measure:"cup"},
+        {recipe_id: 1, ingredient_id:8 , quantity:.5 , unit_of_measure:"cup"},
+        {recipe_id: 1, ingredient_id:9 , quantity:2 , unit_of_measure:"cup"},
+        {recipe_id: 1, ingredient_id:1 , quantity:2 , unit_of_measure:"tablespoon"},
+        {recipe_id: 1, ingredient_id:2 , quantity:.25 , unit_of_measure:"cup"},
+        {recipe_id: 1, ingredient_id:10 , quantity:.5 , unit_of_measure: "teaspoon" },
+        {recipe_id: 1, ingredient_id: 11, quantity:4 , unit_of_measure:"teaspoon"},
+        
       ]);
     });
 };
