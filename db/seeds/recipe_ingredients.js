be34309f85e71c627886ddff7dd06174a9d9a6e4
codejoncode <jonathanjholloway@gmail.com>
2018-09-06 20:44:24 -0500

    recipe_ingredients added for the other dish and recipe associated with it

--- db/seeds/recipe_ingredients.js
+++ db/seeds/recipe_ingredients.js
@@ -20,7 +20,13 @@ exports.seed = function(knex, Promise) {
         {recipe_id: 2, ingredient_id: 11, quantity:4 , unit_of_measure:"teaspoon"},
 
         {recipe_id: 1, ingredient_id: 11, quantity:.5 , unit_of_measure:"cup"},
-        
+        {recipe_id: 1, ingredient_id: 12, quantity:.5, unit_of_measure:"cup"},
+        {recipe_id: 1, ingredient_id: 13, quantity:3, unit_of_measure:"cloves"},
+        {recipe_id: 1, ingredient_id: 14, quantity:1, unit_of_measure:"zested"},
+        {recipe_id: 1, ingredient_id:15 , quantity:.25, unit_of_measure:"cup"},
+        {recipe_id: 1, ingredient_id:16 , quantity: 6, unit_of_measure:"6 ounce"},
+        {recipe_id: 1, ingredient_id:5 , quantity:null, unit_of_measure:"to taste"},
+        {recipe_id: 1, ingredient_id:17 , quantity:null, unit_of_measure:"to taste"}
 
       ]);
     });
