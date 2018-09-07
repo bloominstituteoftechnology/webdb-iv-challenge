f5d8959b71d70f8899c920c42cfb953f822fcfb0
johnoro <officialjohnorourke@gmail.com>
2018-09-06 22:27:20 -0400

    Implemented getRecipe as stretch.

--- api/recipes.js
+++ api/recipes.js
@@ -4,15 +4,19 @@ const db = require('../db/db');
 router.get('/', (req, res) => {
   db.getRecipes()
     .then(recipes => res.status(200).json(recipes))
-    // .catch(err => res.status(500).json(err));
+    .catch(err => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
-  
+  db.getRecipe(req.params.id)
+    .then(recipe => res.status(200).json(recipe))
+    .catch(err => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
-  
+  db.addRecipe(req.body)
+    .then(id => res.status(201).json(id))
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
--- db/db.js
+++ db/db.js
@@ -34,8 +34,41 @@ module.exports = {
 
   getRecipes: () => {
     return db('dishes as d')
-      .select('r.name as recipe', 'd.name as dish')
+      .select('r.id as id', 'r.name as recipe', 'd.name as dish')
       .join('recipes as r', { 'd.id': 'r.dish_id' });
+  },
+
+  getRecipe: (id) => {
+    const recipe = db('recipes as r')
+      .select('r.name as recipe', 'd.name as dish')
+      .join('dishes as d', { 'd.id': 'dish_id' })
+      .where({ 'r.id': id });
+
+    const steps = db('steps as s')
+      .select('step')
+      .join('recipes as r', { 'r.id': 'recipe_id' })
+      .where({ 'r.id': id })
+      .orderBy('order');
+
+    const ingredients = db('ingredients as i')
+      .select('i.name as ingredient', 'quantity')
+      .join('ingredients-for-recipes as ir', { 'ir.id': 'i.id' })
+      .join('recipes as r', { 'r.id': 'ir.id' })
+      .where({ 'r.id': id });
+    
+      return Promise.all([recipe, steps, ingredients])
+        .then(([r, s, i]) => {
+          return r.concat(s, i)
+            .reduce((acc, obj, ind) => {
+              if (ind > 0 && ind < 4) {
+                return Object.assign(acc, { steps: [...acc.steps, obj] });
+              } else if (ind === 4) {
+                return Object.assign(acc, { ingredients: [...acc.ingredients, obj] });
+              } else {
+                return Object.assign(acc, obj);
+              }
+            }, { recipe: '', dish: '', ingredients: [], steps: [] });
+        });
   },
 
   addRecipe: (recipe) => {
