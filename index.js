9f853ec324fad27b1882a0a34667199bc1fd89f2
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-13 15:29:52 -0700

    With Saves

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ res.send('API Running...');
 
 
 
+
+
+
+
+
+
 const port = 2100;
 server.listen(port, function() {
 console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
--- migrations/20180913152005_recipes.js
+++ migrations/20180913152005_recipes.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('recipes', function(tbl){
+
+    tbl
+    .increments();
+
+    tbl
+    .string('name', 128)
+    .notNullable()
+    .defaultTo('not provided');
+
+    tbl
+    .increments();
+
+
+});
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('recipes');
 };
