4706b88b420398419bdec1aa2a63c4fbda3de56a
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-13 15:59:46 -0700

    WIP

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ res.send('API Running...');
 
 
 
+server.get('/dishes', (req, res) => {
+    db('dishes').then(item => {
+        res.status(200).json(item)
+    }).catch((fail) => {
+        console.log(fail);
+        res.status(500).json({ error: "There was an error while receiving the dishes" });
+    })
+})
+
+server.post('/dishes', (req, res) => {
+    const item = req.body;
+
+    db('dishes').insert(item)
+        .then((ids)=> { 
+          res.status(201).json(ids);
+        })
+                .catch((fail) => {
+                    console.log(fail);
+                    res.status(500).json({ error: "There was an error while saving the recipe to the database." });
+                });
+});
+
+
+
 
 
 
--- migrations/20180913152005_recipes.js
+++ migrations/20180913152005_recipes.js
@@ -1,21 +1,7 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('recipes', function(tbl){
-
-    tbl
-    .increments();
-
-    tbl
-    .string('name', 128)
-    .notNullable()
-    .defaultTo('not provided');
-
-    tbl
-    .increments();
-
-
-});
+  
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('recipes');
+  
 };
