e286842791d1a9ac6aa63d0cd610a2640224fa30
marshall <marshall@marshalls-MacBook-Air.local>
2018-10-10 17:34:43 -0700

    got rid of insecurities in knex

--- server.js
+++ server.js
@@ -19,41 +19,36 @@ server.get('/', (req, res) => {
 
 //should return a list of all dishes in the database.
 server.get('/dishes', (req, res) => {
-	knex.raw(`select * from dishes`)
-		.then(response => {
-			console.log(response)
-			res.status(200).json(response);
-		})
+	knex('dishes')
+	.select('*')
+	.then(response => {
+		console.log(response)
+		res.status(200).json(response)
+	})
 })
 
 //should return the dish with the provided id and include a list of the related recipes
 server.get('/dishes/:id', (req, res) => {
 	const id = req.params.id
-	knex.raw(`
-			SELECT dishes.dish_name, recipes.recipe_name
-			FROM dishes
-			INNER JOIN recipes
-			ON dishes.id=recipes.dish_id
-			where recipes.dish_id = ${id}
-		`)
-	.then(response => {
-		console.log(response)
-		res.status(200).json(response)
-	})
+	knex('dishes')
+		.join('recipes', 'dishes.id', '=', 'recipes.dish_id')
+		.select('dishes.dish_name', 'recipes.recipe_name')
+		.where('recipes.dish_id', id)
+		.then(response => {
+			console.log(response)
+			res.status(200).json(response)
+		})
 })
 
 // should return a list of all recipes in the database including the dish they belong to
 server.get('/recipes', (req, res) => {
-	knex.raw(`
-		SELECT dishes.dish_name, recipes.recipe_name
-		FROM dishes
-		INNER JOIN recipes
-		ON dishes.id=recipes.dish_id
-	`)
-	.then(response => {
-		console.log(response)
-		res.status(200).json(response)
-	})
+	knex('dishes')
+		.join('recipes', 'dishes.id', '=', 'recipes.dish_id')
+		.select('dishes.dish_name', 'recipes.recipe_name')
+		.then(response => {
+			console.log(response)
+			res.status(200).json(response)
+		})
 })
 
 /* add a method called getRecipe(id) to your data access library that 
@@ -64,22 +59,23 @@ the list of ingredients with the quantity. */
 
 server.get('/recipes/:id', (req, res) => {
 	id = req.params.id
-	knex.raw(`
-		select dishes.dish_name, recipes.id, recipes.recipe_name, recipes.directions, recipe_ingredients.amount, ingredients.ingredient_name
-		from dishes
-		inner join recipes on recipes.dish_id=dishes.id
-		inner join recipe_ingredients on recipe_ingredients.recipes_id=recipes.id
-		inner join ingredients on recipe_ingredients.ingredient_id=ingredients.id
-		where recipes.id = ${id} 
-	`)
-	.then(response => {
-		console.log(response)
-		res.status(200).json(response);
-	})
+
+	knex('dishes')
+		.join('recipes', 'recipes.dish_id', '=', 'dishes.id')
+		.join('recipe_ingredients', 'recipe_ingredients.recipes_id', '=', 'recipes.id')
+		.join('ingredients', 'recipe_ingredients.ingredient_id', '=', 'ingredients.id')
+		.select('dishes.dish_name', 'recipes.id', 'recipes.recipe_name', 'recipes.directions', 'recipe_ingredients.amount', 'ingredients.ingredient_name')
+		.where('recipes.id', id)
+		.then(response => {
+			console.log(response)
+			res.status(200).json(response);
+		})
 })
 
 //addDish(dish): should add the dish to the database and return the id of the new dish.
 server.post('/dishes/', (req, res) => {
+	const {dish_name} = req.body
+	knex.insert({dish_name}).into('dishes')
 	knex.insert(req.body).into('dishes')
 	.then(response => {
 		console.log(response)
@@ -89,21 +85,17 @@ server.post('/dishes/', (req, res) => {
 
 //addRecipe(recipe): should add a recipe to the database and return the id of the new recipe.
 server.post('/recipes', (req, res) => {
-	knex.insert(req.body).into('recipes')
+	const {recipe_name, directions, dish_id} = req.body
+	knex.insert({recipe_name, directions, dish_id}).into('recipes')
 	.then(response => {
-		console.log(response)
 		res.status(201).json(response)
 	})
 })
 
 
+
+
 server.listen(port, () => console.log(`server running on port 5555`));
-
-
-
-
-
-
 
 
 
