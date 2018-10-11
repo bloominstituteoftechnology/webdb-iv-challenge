15dfc022ba3d888a6ddd947c3a8e57e2050e5418
Nicocchi <nicocchilover@gmail.com>
2018-10-11 17:16:01 -0400

    added database

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/dev.sqlite3'
+      filename: './data/recipebook.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
