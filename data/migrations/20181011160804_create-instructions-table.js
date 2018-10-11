2e8e452df052d41859e1018f207161b3fc024393
unknown <cornielleandres@gmail.com>
2018-10-11 16:10:30 -0400

    Create instructions table with migrations.

---
+++ data/migrations/20181011160804_create-instructions-table.js
@@ -0,0 +1,21 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('instructions', function(table) {
+		table
+			.increments();
+
+		table
+			.string('step', 255)
+			.notNullable();
+
+		table
+			.integer('recipe_id')
+			.notNullable()
+			.unsigned()
+			.references('id')
+			.inTable('recipes');
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('instructions');
+};
