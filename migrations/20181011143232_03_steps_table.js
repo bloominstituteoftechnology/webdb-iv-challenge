09c9d958fa8fcdd4b6fd6e0a170c840cc3bddcc1
m-j-hacker <mjhacker@gmail.com>
2018-10-11 14:52:23 -0500

    created steps migration table

--- migrations/20181011143232_03_steps_table.js
+++ migrations/20181011143232_03_steps_table.js
@@ -1,6 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('steps', function(tbl) {
+      tbl.increments();
+
+      tbl.integer('step_number').notNullable();
+
+      tbl
+      .string('instructions')
+      .notNullable()
+      .references('step_number')
+  });
 };
 
 exports.down = function(knex, Promise) {
