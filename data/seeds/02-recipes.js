ee50f52561e145f2fa45a7d4017209024c3ee4ba
Timothy Hoang <timh1203@gmail.com>
2018-10-11 18:38:35 -0400

    Add seed 02 recipes

---
+++ data/seeds/02-recipes.js
@@ -0,0 +1,18 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('recipes')
+    .truncate()
+    .then(function () {
+      return knex('recipes').insert([
+        {name: 'Supreme', dish_id: '1'},
+        {name: 'Hawaiian', dish_id: '1'},
+        {name: 'Meats', dish_id: '1'},
+        {name: 'Beef', dish_id: '2'},
+        {name: 'Chicken', dish_id: '2'},
+        {name: 'Veggie', dish_id: '2'},
+        {name: 'Angus', dish_id: '3'},
+        {name: 'Buffalo', dish_id: '3'},
+        {name: 'Vegetarian', dish_id: '3'},
+      ]);
+    });
+};
