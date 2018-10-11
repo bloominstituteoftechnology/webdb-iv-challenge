4df30fe173c351ffdd25dcbb0b4c17660689a771
Timothy Hoang <timh1203@gmail.com>
2018-10-11 18:47:07 -0400

    Add seed 04 ingredients

---
+++ data/seeds/04-ingredients.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('ingredients')
+    .truncate()
+    .then(function () {
+      return knex('ingredients').insert([
+        {name: 'pepperoni'},
+        {name: 'ground beef'},
+        {name: 'green peppers'},
+        {name: 'onions'},
+        {name: 'tomatoes'},
+        {name: 'mushrooms'},
+      ]);
+    });
+};
