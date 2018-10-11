0b905951574749759be35e39c9fc5f088f1e7fb4
alejandrok93 <alejandrok93@gmail.com>
2018-10-11 14:29:33 -0700

    Added data modeil file to perform CRUD operations on dishes and recipes db

---
+++ data/models/dataModel.js
@@ -0,0 +1,27 @@
+const knex = require("knex");
+const knexConfig = require("../../knexfile.js");
+const db = knex(knexConfig.development);
+
+const getDishes = () => {
+  return db("dishes");
+};
+
+const getDish = id => {
+  return db("dishes")
+    .where({ id })
+    .first();
+};
+
+const addDish = dish => {
+  return db("dishes").insert(dish);
+};
+
+const getRecipes = () => {
+  return db("recipes");
+};
+
+const addRecipe = recipe => {
+  return db("recipes").insert(recipe);
+};
+
+module.exports = { getDishes, getDish, addDish, getRecipes, addRecipe };
--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const express = require("express");
-
+const db = require("./data/models/dataModel.js");
 const server = express();
+
+//apply middleware
+server.use(express.json());
+
+//Routes
+server.get("/api/dishes", (req, res) => {
+  db.getDishes()
+    .then(dishes => res.status(200).json(dishes))
+    .catch(err => res.send(err));
+});
+
+server.post("/api/dishes", (req, res) => {
+  const dish = req.body;
+  console.log(dish);
+  db.addDish(dish)
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(200).json(err));
+});
+
+server.get("/api/dishes/:id", (req, res) => {
+  id = req.params.id;
+  if (!id) {
+    res.status(400).json({ error: "Bad request" });
+  }
+  db.getDish(id)
+    .then(dish => {
+      if (!dish) {
+        res
+          .status(500)
+          .json({ error: "There was an error retrieving the dish" });
+      } else res.status(200).json(dish);
+    })
+    .catch(err => res.send(err));
+});
+
+//--------RECIPE ROUTES _--------
+server.get("/api/recipes", (req, res) => {
+  db.getRecipes()
+    .then(recipes => res.status(200).json(recipes))
+    .catch(err => res.send(err));
+});
+
+server.post("/api/drecipes", (req, res) => {
+  const recipe = req.body;
+  console.log(recipe);
+  db.addRecipe(recipe)
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(200).json(err));
+});
 
 server.listen(9000, () => {
   console.log("API is running");
