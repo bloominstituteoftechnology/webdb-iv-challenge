0c7b8807cfa7f71806874c263fc52d971a2aa9e1
Clint Kunz <clintkunz@gmail.com>
2018-10-11 15:31:40 -0600

    steps migration

---
+++ data/migrations/20181011152903_steps_table.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('steps', function(tbl) {
+        tbl.increments();
+        tbl.string('step', 280).notNullable();
+        tbl.integer('recipe_id').unsigned().references('id').inTable('recipes')
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('steps')
+};
