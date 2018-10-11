2f5943c35972d0a9d6825cf66f156bb1a4fe40ca
Clint Kunz <clintkunz@gmail.com>
2018-10-11 15:37:32 -0600

    unit of measurement migration

---
+++ data/migrations/20181011153620_unit_of_measurement_table.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('UofM', function(tbl) {
+        tbl.increments();
+        tbl.string('Unit', 64).notNullable();
+        tbl.integer('quantity_id').unsigned().references('id').inTable('quantity');
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('UofM')
+};
