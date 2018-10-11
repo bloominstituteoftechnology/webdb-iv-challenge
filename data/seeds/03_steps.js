711193719c06e37e67c1e1197493960184d8a215
m-j-hacker <mjhacker@gmail.com>
2018-10-11 15:09:11 -0500

    bugfixes to 03_steps.js

--- data/seeds/03_steps.js
+++ data/seeds/03_steps.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('steps').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('steps').insert([
         {step_number: 1, instructions: 'This is where', recipe_id: 1},
         {step_number: 2, instructions: 'recipe steps', recipe_id: 1},
         {step_number: 3, instructions: 'will be kept', recipe_id: 1}
