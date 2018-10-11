8f0c7bb41a2c6a43f5faabd43596876794d5911d
Andrew <ghobrial.andrew@gmail.com>
2018-10-11 18:34:46 -0400

    completed all migration logic besides steps_table

--- data/migrations/20181011174316_ingredients_table.js
+++ data/migrations/20181011174316_ingredients_table.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("ingredients", function(tbl) {
+    tbl.increments();
+     tbl.string("name").notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("ingredients");
 };
