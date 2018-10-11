d120b0029031e3c5bf2655711e2c17c6a37f7312
m-j-hacker <mjhacker@gmail.com>
2018-10-11 16:10:30 -0500

    created getDishes, getDish(id) and addDish(dish) to helperFunctions

--- helperFunctions.js
+++ helperFunctions.js
@@ -14,3 +14,16 @@ module.exports = {
 function getDishes() {
     return db('dishes');
 }
+
+function getDish(id) {
+    return db('dishes')
+        .join('recipes', 'dishes.id', '=', 'recipes.dish_id')
+        .where('dishes.id', id)
+        .select('dishes.name as name', 'recipes.name as recipe')
+}
+
+function addDish(dish) {
+    return db('dishes')
+    .insert(dish)
+    .into('dishes');
+}
