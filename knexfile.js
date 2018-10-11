8f5d93b20a252d6ebe2f1f37b561d915122c2196
m-j-hacker <mjhacker@gmail.com>
2018-10-11 15:52:52 -0500

    changed migrations directory

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds',
