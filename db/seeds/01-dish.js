157d1aee67cfd919689641e67eb1d2aa5c8806d2
Csillag61 <csillagok@gmail.com>
2018-10-11 17:22:28 -0400

    Seeds added

---
+++ db/seeds/01-dish.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('dish')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('dish').insert([
+        {name: 'soup'},
+        { name: 'tacos'},
+        { name: 'pizza'}
+      ]);
+    });
+};
---
+++ db/seeds/04-instructions.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('instructions').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("instructions").insert([
+        { recipe_id: 1, step: 1, instruction: "Get ingredients" },
+        { recipe_id: 1, step: 2, instruction: "Mix ingredients" },
+        { recipe_id: 1, step: 3, instruction: "Cook ingredients" },
+        { recipe_id: 2, step: 1, instruction: "Get ingredients" },
+        { recipe_id: 2, step: 2, instruction: "Mix ingredients" },
+        { recipe_id: 2, step: 3, instruction: "Cook ingredients" },
+        { recipe_id: 3, step: 1, instruction: "Get ingredients" },
+        { recipe_id: 3, step: 2, instruction: "Mix ingredients" },
+        { recipe_id: 3, step: 3, instruction: "Cook ingredients" }
+      ]);
+    });
+};
