bda61808fd3a64b02207e8f49e4380ea1f8a7a9f
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-11 17:50:48 -0500

    Title

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const port = 5000;
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+//Testing 
+server.get('/', (req, res) => {
+    res.send('Am I Alive????'); // Yes!
+});
+
+server.listen(port, () => console.log(`\n === Listening on Port ${port} === \n`));
