e8e0a0ebf663c31dad3e9f79ee1300fd4b14096c
Nicocchi <nicocchilover@gmail.com>
2018-10-11 17:10:15 -0400

    added steps schema and fixed steps seed

--- data/migrations/20181011164553_steps_table.js
+++ data/migrations/20181011164553_steps_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('steps', tbl => {
+        tbl.increment();
+        tbl.integer('recipe_id').unsigned().reference('id').inTable('recipe');
+        tbl.integer('order').notNullable();
+        tbl.string('description', 255).notNullable();
+    })
+  };
   
-};
-
-exports.down = function(knex, Promise) {
+  exports.down = function(knex, Promise) {
+    return knex.schema.DropTableIfExists('steps');
+  };
   
-};
--- data/seeds/04-steps.js
+++ data/seeds/04-steps.js
@@ -5,15 +5,14 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('steps').insert([
-        {recipe_id: 1, order: 1, ingredient: 'Heat oven to 250°F. In large skillet, brown ground beef and onion over medium heat for 8 to 10 minutes or until beef is thoroughly cooked, stirring frequently. Drain. '},
-        {recipe_id: 1, order: 2, ingredient: 'Stir in chili powder, salt, garlic powder and tomato sauce. Reduce heat to low; cover and simmer 10 minutes. '},
-        {recipe_id: 1, order: 3, ingredient: 'Meanwhile, place taco shells on ungreased cookie sheet. Heat at 250°F. for 5 minutes.'},
-        {recipe_id: 1, order: 4, ingredient: 'To assemble tacos, layer beef mixture, cheese, lettuce and tomatoes in each taco shell. Serve with salsa; top with sour cream.'},
-       
-        {recipe_id: 2, order: 1, ingredient: 'Cook rice according packet instructions. Drain and cool.'},
-        {recipe_id: 2, order: 2, ingredient: 'Heat ½ oil in wok or large frying pan; add eggs; swirl over base to form an omelette; cook until firm. Remove egg from wok, roll up firmly, and cut into thin slices.'},
-        {recipe_id: 2, order: 3, ingredient: 'Chop meat and vegetables into 2cm pieces.'},
-        {recipe_id: 2, order: 4, ingredient: 'Heat remaining oil in pan add meat/fish cook for 1 minutes, add vegetables, heat for 1 minute add rice, egg and MAGGI Original Seasoning. Stir until heated through.'},
+        {recipe_id: 1, order: 1, description: 'Heat oven to 250°F. In large skillet, brown ground beef and onion over medium heat for 8 to 10 minutes or until beef is thoroughly cooked, stirring frequently. Drain. '},
+        {recipe_id: 1, order: 2, description: 'Stir in chili powder, salt, garlic powder and tomato sauce. Reduce heat to low; cover and simmer 10 minutes. '},
+        {recipe_id: 1, order: 3, description: 'Meanwhile, place taco shells on ungreased cookie sheet. Heat at 250°F. for 5 minutes.'},
+        {recipe_id: 1, order: 4, description: 'To assemble tacos, layer beef mixture, cheese, lettuce and tomatoes in each taco shell. Serve with salsa; top with sour cream.'},
+        {recipe_id: 2, order: 1, description: 'Cook rice according packet instructions. Drain and cool.'},
+        {recipe_id: 2, order: 2, description: 'Heat ½ oil in wok or large frying pan; add eggs; swirl over base to form an omelette; cook until firm. Remove egg from wok, roll up firmly, and cut into thin slices.'},
+        {recipe_id: 2, order: 3, description: 'Chop meat and vegetables into 2cm pieces.'},
+        {recipe_id: 2, order: 4, description: 'Heat remaining oil in pan add meat/fish cook for 1 minutes, add vegetables, heat for 1 minute add rice, egg and MAGGI Original Seasoning. Stir until heated through.'},
       ]);
     });
 };
