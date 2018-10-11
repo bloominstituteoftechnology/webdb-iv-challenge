7fd89acbd6dcb360592afe5f937f7b8992064143
decagondev <decagondevelopment@gmail.com>
2018-10-11 21:31:35 +0100

    (MVP) : add migradtion content for dish and recipes

--- data/migrations/20181011210413_create_dish_table.js
+++ data/migrations/20181011210413_create_dish_table.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("dishes", function(table) {
+    table.increments();
+    table
+      .string("dish_name")
+      .notNullable()
+      .unique("dish_name");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("dishes");
 };
--- data/migrations/20181011210449_create_recipes_table.js
+++ data/migrations/20181011210449_create_recipes_table.js
@@ -1,8 +1,23 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("recipes", function(table) {
+    table.increments();
+
+    table
+      .string("recipe_name")
+      .notNullable()
+      .unique("recipe_name");
+
+    table.string("imstructions").notNullable();
+
+    table
+      .integer("dish_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("dishes");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("recipes");
 };
