96811e169b3c8c188a9738fff8bbabc5b58e75cc
Timothy Hoang <timh1203@gmail.com>
2018-10-11 18:32:35 -0400

    Add seed 01 dishes

---
+++ data/seeds/01-dishes.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('dishes')
+    .truncate()
+    .then(function () {
+      return knex('dishes').insert([
+        {name: 'Pizza'},
+        {name: 'Tacos'},
+        {name: 'Burgers'}
+      ]);
+    });
+};
