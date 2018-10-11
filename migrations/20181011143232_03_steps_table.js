fad1a5b7187e4b20ea6493db9c35a70b014c5aa7
m-j-hacker <mjhacker@gmail.com>
2018-10-11 14:56:48 -0500

    created ingredients migration table

--- migrations/20181011143232_03_steps_table.js
+++ migrations/20181011143232_03_steps_table.js
@@ -13,5 +13,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('steps');
 };
--- migrations/20181011143522_04_ingredients_table.js
+++ migrations/20181011143522_04_ingredients_table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('ingredients', function(tbl) {
+      tbl.increments();
+
+      tbl.string('name').notNullable();
+
+      tbl
+        .integer('recipe_id')
+        .unsigned()
+        .references('id')
+        .inTable('recipes')
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('ingredients');
 };
