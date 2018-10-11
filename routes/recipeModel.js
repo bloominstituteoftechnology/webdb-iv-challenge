46b20679d2eae05aab0a035b80955c58e10fc4db
Nicocchi <nicocchilover@gmail.com>
2018-10-11 18:11:13 -0400

    added the rest of the dish routes and recipe routes

--- routes/recipeModel.js
+++ routes/recipeModel.js
@@ -1,34 +1,34 @@
 const knex = require('knex');
 
-const knexConfig = require('../../knexfile.js');
+const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
 module.exports = {
-  get,
-  getById,
-  post,
-  update,
-  remove,
+  getDishes,
+  getDish,
+  addDish,
+  getRecipes,
+  addRecipe,
 };
 
 // Returns all the dishes
-function getDish() {
+function getDishes() {
   return db('dish');
 }
 
 // Returns the dish with the corresponding ID
-function getDishe(id) {
-  return db('dish').where({ id }).first();
+function getDish(id) {
+  return db.select('*').from('dish').where('dish.id', id).leftJoin('recipe', 'dish.id', 'recipe.dish_id');
 }
 
 // Inserts the new dish, returns 1
 function addDish(dish) {
-  return db('dish').insert(dish);
+  return db('dish').insert(dish).into('dish');
 }
 
 // Returns all the recipes
 function getRecipes() {
-    return db('recipe');
+    return db.select('*').from('recipe').leftJoin('dish', 'recipe.dish_id', 'dish.id');
 }
 
 // Inserts the new recipe, returns 1
