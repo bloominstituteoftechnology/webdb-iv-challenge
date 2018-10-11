e02174dde48222108ece93c0f42e4b2399c1f88f
decagondev <decagondevelopment@gmail.com>
2018-10-11 21:39:00 +0100

    (MVP) : add migradtion content for recipe_ingredient table

--- data/migrations/20181011210512_create_recipe_ingredient_table.js
+++ data/migrations/20181011210512_create_recipe_ingredient_table.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("recipe_ingredient", function(table) {
+    table.increments();
+
+    table.string("amount").notNullable();
+
+    table
+      .integer("recipes_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("recipes");
+
+    table
+      .integer("ingredient_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("ingredient");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("recipe_ingredient");
 };
