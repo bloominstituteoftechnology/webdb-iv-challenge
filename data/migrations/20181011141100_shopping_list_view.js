a856b422a1b387fa8c88d1769a7f55025e394ac9
Vera Simon <ghjkl>
2018-10-12 08:04:37 -0700

    DB Recipe Book
    Fixed a bug in the View select. A Group By had snuck in inadvertantly

--- data/migrations/20181011141100_shopping_list_view.js
+++ data/migrations/20181011141100_shopping_list_view.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-    return knex.raw('create view shopping_lists as SELECT recipes.name AS Recipe, ingredients.name AS Ingredient, recipe_ingredients.amount AS Amount, recipe_ingredients.unit AS Type FROM recipe_ingredients INNER JOIN ingredients ON ingredients.id = recipe_ingredients.ingredient INNER JOIN recipes ON recipes.id = recipe_ingredients.recipe GROUP BY recipes.name');
+    return knex.raw('create view shopping_lists as SELECT recipes.name AS Recipe, ingredients.name AS Ingredient, recipe_ingredients.amount AS Amount, recipe_ingredients.unit AS Type FROM recipe_ingredients INNER JOIN ingredients ON ingredients.id = recipe_ingredients.ingredient INNER JOIN recipes ON recipes.id = recipe_ingredients.recipe');
 };
 
 exports.down = function(knex, Promise) {
