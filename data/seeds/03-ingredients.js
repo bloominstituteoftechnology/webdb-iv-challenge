c01b982dcd57582eb8d268fe4998e604d3f4320e
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-12 09:30:47 -0400

    Added data to all tables

--- data/seeds/03-ingredients.js
+++ data/seeds/03-ingredients.js
@@ -5,13 +5,13 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("ingredients").insert([
-        { name: "tortillas", dish_id: 1 },
-        { name: "chicken", dish_id: 1 },
-        { name: "flour", dish_id: 2 },
-        { name: "salsa", dish_id: 2 },
-        { name: "mushrooms", dish_id: 3 },
-        { name: "marinara sauce", dish_id: 3 },
-        { name: "cheese", dish_id: 3 }
+        { ingredient: "tortillas" },
+        { ingredient: "shrimp" },
+        { ingredient: "marinara sauce" },
+        { ingredient: "cheese" },
+        { ingredient: "mushrooms" },
+        { ingredient: "chicken" },
+        { ingredient: "carrots" }
       ]);
     });
 };
