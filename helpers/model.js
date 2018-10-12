328297f8def2ca8cd1b2de1ab09be92854146674
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-11 22:10:59 -0700

    Model done

--- helpers/model.js
+++ helpers/model.js
@@ -1,3 +1,7 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+
 module.exports = {
   getDishes,
   addDish,
@@ -13,23 +17,34 @@ function getDishes() {
 function addDish(dish) {
   return db('dishes')
     .insert(dish)
-    .into('dishes');
+    .into('dishes')
+    .then(ids => ({ id: ids[0]}));
+    
 }
 
 function getDish(id) {
   return db('dishes')
     .where({ id })
-    .first();
+    .first()
+    .join('recipies', 'dishes.id', '=', 'recipies.dish_id');
 }
 
+// select d.dishName, r.recipeName
+// from dishes as d
+//   join recipies as r
+//   on d.id = r.dish_id
+
 function getRecipies() {
-  return db('recipies');
+  return db('recipies')
+    .join('dishes', 'recipies.dish_id', '=', 'dishes.id')
+    .select('recipies.recipeName', 'dishes.dishName');
 }
 
 function addRecipe(recipe) {
   return db('recipies')
     .insert(recipe)
-    .into('recipies');
+    .into('recipies')
+    .then(ids => ({ id: ids[0]}));
 }
 
 // repository pattern for data access
