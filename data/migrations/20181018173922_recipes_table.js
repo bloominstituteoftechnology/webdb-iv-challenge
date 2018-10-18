846d9d67a4ff02fdcae47cd47a073dc462e466fd
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-18 17:47:52 -0500

    fixed path in knexfile. create recipes table

---
+++ data/migrations/20181018173922_recipes_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("recipes", function(tbl){
+        tbl.increments();
+        tbl.string('name').notNullable();
+        tbl.integer('dish_id').unsigned().references('id').inTable('dishes');
+    });
+}
+
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("recipes")
+};
