7d077b3152f343b45acc161d00a49b27dfb7cc80
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-18 17:38:39 -0500

    dishes table migration added

---
+++ data/migrations/20181018172822_dishes_table.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('dishes', function(tbl){
+    tbl.increments();
+    tbl.string('name').notNullable();
+    tbl.unique('name');
+  });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('dishes');
+};
