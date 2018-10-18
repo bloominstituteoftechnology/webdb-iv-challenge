cf8108e433ef0bd7b5d21804bee44d0d4d4508c0
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-18 17:52:55 -0500

    created ingredients table

---
+++ data/migrations/20181018174900_ingredients_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('ingredients', function(tbl){
+        tbl.increments();
+        tbl.string('name').notNullable();
+        tbl.unique('name');
+        tbl.integer('recipe_id').unsigned().references('id').inTable('recipes')
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('ingredients');
+};
