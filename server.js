2e51442e3aeb8d7c8d696fa55e95bc3bf2b94664
verydecent <wnjhwng@gmail.com>
2018-10-18 13:16:09 -0700

    tests server

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+server.get('/', (req, res) => {
+    res.json({ test: `success on PORT ${port}`})
+})
+
 const port = 4234;
 server.listen(port, console.log(`====== API ${port} ======`));
