533d37e4c4d106e503ac5361dd13433d983bcc56
IlanL75 <anatoly.leytman@gmail.com>
2018-10-18 12:57:10 -0700

    Adding migrations for dishes, ingredients, and recipes

---
+++ data/migrations/20181018113729_dishes_table.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('dishes', function(tbl) {
+        tbl.increments().primary();
+        tbl.string('name', 255).notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('dishes');
+};
