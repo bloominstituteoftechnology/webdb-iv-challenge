39b206297a3ed0e8b04c7dc9c201e670df910d33
Lee Len Formento <leebatef@gmail.com>
2018-11-13 18:21:49 -0500

    Migrations and Seeds for Dishes. Also GET to see if data renders

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
-// const knex= require('knex');
+const knex= require('knex');
 
-// const knexConfig = require('./knexfile');
-// const db = knex(knexConfig.development); 
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development); 
 
 const server = express();
 
@@ -13,5 +13,16 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send("It's Alive!");
 })
+
+server.get('/api/dishes', (req, res) => {
+    db('dishes')
+    .then(names => {
+      console.log(names)
+      res.status(200).json(names)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
+  });
 
 server.listen(7200, () => console.log('\n Party at port 7200 '))
