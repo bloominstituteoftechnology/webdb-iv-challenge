ef8fa711b56538142ce04ec7011bb9bdcc076d26
ryanboris <ryan.boris@gmail.com>
2018-11-14 17:24:45 -0500

    Creates the remaining dishes and recipe tables

---
+++ data/migrations/20181114171641_add_dishes_recipes_table.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('dishes_recipes', function(table) {
+		table.increments()
+		table.integer('dish_id').notNullable().unsigned().references('id').inTable('dishes')
+
+		table.integer('recipe_id').notNullable().unsigned().references('id').inTable('recipes')
+
+		table.unique('recipe_id')
+	})
+}
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('dishes_recipes')
+}
