85d54bfd2afb766227ecae3139ac51da7f83cbaa
ryanboris <ryan.boris@gmail.com>
2018-11-14 17:14:06 -0500

    Creates and migrates dishes, ingredients, instructions

--- data/migrations/20181114125657_add_ingredients_table.js
+++ data/migrations/20181114125657_add_ingredients_table.js
@@ -1,16 +1,11 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('ingredients', function(table) {
+		table.increments()
+		table.string('name', 255).notNullable()
+		table.unique('name')
+	})
+}
 
-exports.up = function (knex, Promise) {
-    return knex.schema.createTable('ingredients', function (table) {
-        table.increments()
-        table.string('ingredient', 255).notNullable().unique()
-
-
-    })
-
-};
-
-exports.down = function (knex, Promise) {
-
-    return knex.schema.dropTableIfExists('ingredients')
-
-};
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('ingredients')
+}
