4b95d85837af5c4638e1d8af1864df4dacdc56a7
Lee Len Formento <leebatef@gmail.com>
2018-11-13 21:32:41 -0500

    Commenting out & deleting soon test GETs to work on Data access file creation

--- index.js
+++ index.js
@@ -14,49 +14,49 @@ server.get('/', (req, res) => {
     res.send("It's Alive!");
 })
 
-server.get('/api/dishes', (req, res) => {
-    db('dishes')
-    .then(names => {
-      console.log(names)
-      res.status(200).json(names)
-    })
-    .catch(err => {
-      res.status(500).json(err)
-    });
-  });
+// server.get('/api/dishes', (req, res) => {
+//     db('dishes')
+//     .then(names => {
+//       console.log(names)
+//       res.status(200).json(names)
+//     })
+//     .catch(err => {
+//       res.status(500).json(err)
+//     });
+//   });
 
 
-server.get('/api/recipes', (req, res) => {
-    db('recipes')
-    .then(names => {
-      console.log(names)
-      res.status(200).json(names)
-    })
-    .catch(err => {
-      res.status(500).json(err)
-    });
-  });
+// server.get('/api/recipes', (req, res) => {
+//     db('recipes')
+//     .then(names => {
+//       console.log(names)
+//       res.status(200).json(names)
+//     })
+//     .catch(err => {
+//       res.status(500).json(err)
+//     });
+//   });
 
-  server.get('/api/ingredients', (req, res) => {
-    db('ingredients')
-    .then(ingr => {
-      console.log(ingr)
-      res.status(200).json(ingr)
-    })
-    .catch(err => {
-      res.status(500).json(err)
-    });
-  });
+//   server.get('/api/ingredients', (req, res) => {
+//     db('ingredients')
+//     .then(ingr => {
+//       console.log(ingr)
+//       res.status(200).json(ingr)
+//     })
+//     .catch(err => {
+//       res.status(500).json(err)
+//     });
+//   });
 
-  server.get('/api/steps', (req, res) => {
-    db('stepstry')
-    .then(steps=> {
-      console.log(steps)
-      res.status(200).json(steps)
-    })
-    .catch(err => {
-      res.status(500).json(err)
-    });
-  });
+//   server.get('/api/steps', (req, res) => {
+//     db('stepstry')
+//     .then(steps=> {
+//       console.log(steps)
+//       res.status(200).json(steps)
+//     })
+//     .catch(err => {
+//       res.status(500).json(err)
+//     });
+//   });
 
 server.listen(7200, () => console.log('\n Party at port 7200 '))
