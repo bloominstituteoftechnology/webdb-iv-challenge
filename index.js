467ab3e679ec4ba08b65ab9f9a61a58e044244c2
Lee Len Formento <leebatef@gmail.com>
2018-11-13 22:45:23 -0500

    ADD recipe and dish

--- index.js
+++ index.js
@@ -54,4 +54,30 @@ getRecipe = server.get('/api/recipes/:id', (req, res) => {
     res.status(500).json(err));
 });
 
+addDish = server.post('/api/dishes', (req, res) => {
+  const name = req.body;
+   db
+   .insert(name) 
+   .into('dishes')
+  .then(name => {
+    res.status(200).json(name[0])
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+})
+
+addRecipe = server.post('/api/recipes', (req, res) => {
+  const name = req.body;
+   db
+   .insert(name) 
+   .into('recipes')
+  .then(name => {
+    res.status(200).json(name[0])
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+})
+
 server.listen(7200, () => console.log('\n Party at port 7200 '))
