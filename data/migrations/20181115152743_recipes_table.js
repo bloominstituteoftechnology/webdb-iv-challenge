6c4812e34161781d40c951e7c9ec88ba52cd2ccb
Yanrong Xu <xuyanrong@icloud.com>
2018-11-15 15:31:06 -0500

    create recipes table

---
+++ data/migrations/20181115152743_recipes_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipes', function(tbl){
+      tbl.increments();
+      tbl.string('name', 255).notNullable();
+      tbl
+        .integer('dish_id')
+        .unsigned()
+        .references('id')
+        .inTable('dishes')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('recipes');
+};
