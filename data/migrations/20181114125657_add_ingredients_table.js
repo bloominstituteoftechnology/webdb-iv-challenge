f625cee5cbe58437fcd603ca522b06f95d2d12eb
ryanboris <ryan.boris@gmail.com>
2018-11-15 06:08:55 -0500

    Creates data access methods to complete MVP

--- data/migrations/20181114125657_add_ingredients_table.js
+++ data/migrations/20181114125657_add_ingredients_table.js
@@ -1,11 +1,17 @@
-exports.up = function(knex, Promise) {
-	return knex.schema.createTable('ingredients', function(table) {
-		table.increments()
-		table.string('name', 255).notNullable()
-		table.unique('name')
-	})
-}
 
-exports.down = function(knex, Promise) {
-	return knex.schema.dropTableIfExists('ingredients')
-}
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('ingredients', function (table) {
+        table.increments()
+        table.string('name', 128).notNullable().unique()
+
+
+
+    })
+
+};
+
+exports.down = function (knex, Promise) {
+
+    return knex.schema.dropTableIfExists('ingredients')
+
+};
