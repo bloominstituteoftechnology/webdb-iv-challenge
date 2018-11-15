3c15bfdcae2f4d1ddf4933e880abb88b13d0a68a
Gannon <gkleblanc@gmail.com>
2018-11-15 16:22:45 -0500

    added Dish migration and seed data

---
+++ data/migrations/20181115161231_Dish_Table.js
@@ -0,0 +1,15 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('Dish', function (tbl) {
+
+        tbl.increments()
+
+        tbl
+            .string('name', 200)
+            .notNullable();
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('Dish');
+};
