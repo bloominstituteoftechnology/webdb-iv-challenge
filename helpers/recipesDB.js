faf62fba2234c040a18f525b864d469a4803bc39
bkwillia052 <bkwillia052@gmail.com>
2018-11-15 17:48:40 -0600

    DB helper file and functions created. Get method on server is running successfully

--- helpers/recipesDB.js
+++ helpers/recipesDB.js
@@ -4,19 +4,19 @@ const db = knex(knexConfig.development);
 
 module.exports = {
     getDishes: function(){
-
+        return db('dishes')
     },
     addDish: function(dish){
-
+        return db('dishes').insert(dish)
     },
     getDish: function(id){
-
+        return db('dishes').where(id)
     },
     getRecipes: function(){
-
+        return db('recipes');
     },
     addRecipe: function(recipe){
-        
+        return db('recipes').insert(recipe)
     }
 
 
--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const db = require('./helpers/recipesDB')
 server.use(express.json());
 
 
+server.get('/dishes', (req,res) =>{
+    db.getDishes()
+    .then(r => r.length ? res.status(200).json(r) : res.status(404).json({message: 'Data not found'}))
+    .catch(err => res.status(500).json({message: 'An error occurred while retrieving the data.'}))
 
+})
 
 
 server.listen(8888, ()=> console.log('Server listening on Port 8888'));
