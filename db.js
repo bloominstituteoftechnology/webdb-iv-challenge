4fb4e13e1d407a3b1a3251d65eb6c3380a68e9bf
DaftBeowulf <ajmboosh@gmail.com>
2018-11-15 17:33:37 -0600

    MVP possibly complete, need to test functions from db.js to see if they work as intended via endpoints in index.js

---
+++ db.js
@@ -0,0 +1,43 @@
+//import knex and initialize database
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+const db = knex(knexConfig.development);
+
+//export all custom functions on the various tables
+module.exports = {
+  getDishes,
+  addDish,
+  getDish,
+  getRecipes,
+  addRecipe
+};
+
+//custom functions
+function getDishes() {
+  return db("dishes");
+}
+
+function addDish(dish) {
+  return db("dishes")
+    .insert(dish)
+    .then(ids => ({ id: ids[0] }));
+}
+
+function getDish(id) {
+  return db("dishes")
+    .join("recipes", "dishes.id", "recipes.dish_id")
+    .select("dishes.name", "recipes.name as recipes")
+    .where("dishes.id", id);
+}
+
+function getRecipes() {
+  return db("recipes")
+    .join("dishes", "recipes.dish_id", "dishes.id")
+    .select("recipes.name", "dishes.name as dish");
+}
+
+function addRecipe(recipe) {
+  return db("recipes")
+    .insert(recipe)
+    .then(ids => ({ id: ids[0] }));
+}
