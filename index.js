2fa618c25d6a2fd4c137989fe82578c264b77461
adamhinckley <adamhinckley@mac.com>
2018-11-15 14:10:44 -0700

    dependencies installed, server wired up, knexfile wired up

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(5000, () => console.log("\n== Port 5k ==\n"));
