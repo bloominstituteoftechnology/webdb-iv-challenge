0bc551cdfcca86aea3a1ce9f7f5cacebc5c87aee
Matthew Wright <matthewbwright@gmail.com>
2018-11-15 15:05:17 -0700

    adds recipes table

--- data/migrations/20181115142104_recipes_table.js
+++ data/migrations/20181115142104_recipes_table.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipes', tbl => {
+    tbl.increments();
+    tbl.string('name').notNullable();
+    tbl.string('directions');
+    tbl.string('ingredients');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('recipes');
 };
