38f0dc7bf91166a88ffbb2e8aef7be725d3cd585
AM_Myrick <amyrick.820@gmail.com>
2018-11-15 14:53:23 -0500

    creates ingredients migration

--- Data/migrations/20181115144008_ingredients_table.js
+++ Data/migrations/20181115144008_ingredients_table.js
@@ -1,8 +1,10 @@
-
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('ingredients', function (table) {
+        table.increments()
+        table.string('name', 50).notNullable()
+        table.float("amount needed").notNullable()
+     })
+ };
+ exports.down = function (knex, Promise) {
+     return knex.schema.dropTableIfExists('ingredients')
+ };
