f695732518eb2e24ef95e496771b0a74462b8fe2
Drewsup123c <drewsup123c@gmail.com>
2018-11-15 16:13:25 -0700

    got rid of some code in index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const express = require('express')
-const knex = require('knex');
-const knexConfig = require('./knexfile.js');
-const db = knex(knexConfig.development);
 const server = express();
 const port = 9000;
 server.use(express.json());
