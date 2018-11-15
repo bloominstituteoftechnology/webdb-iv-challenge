7907a88ae5dbf8c8d5ddecb64a10fee92aca5105
AM_Myrick <amyrick.820@gmail.com>
2018-11-15 14:49:27 -0500

    creates recipes migration

--- Data/migrations/20181115143949_recipes_table.js
+++ Data/migrations/20181115143949_recipes_table.js
@@ -1,8 +1,9 @@
-
 exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+	return knex.schema.createTable('recipes', function(table) {
+		table.increments()
+ 		table.string('name', 128).notNullable()
+	})
+}
+ exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('recipes')
+}
