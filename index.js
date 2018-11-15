6ea36b03bbb8d3ac64b8e8d62622bd5aca13db17
AM_Myrick <amyrick.820@gmail.com>
2018-11-15 16:59:30 -0500

    completes dbHelper and test endroutes

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require("knex");
 const knexConfig = require("./knexfile")
-
-const db = knex(knexConfig.development)
+const db = require("./Data/dbHelper.js")
 
 const server = express();
 
@@ -13,6 +12,52 @@ server.use(helmet());
 server.get("/", (req, res) => {
     res.status(200).json({api: "running"});
 })
+
+server.get('/api/dishes', (req, res) => {
+    db.getDishes()
+        .then(dishes => res.status(200).json(dishes))
+        .catch(err => res.status(500).json({error: err}))
+});
+
+server.get('/api/dishes/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.getDish(id)
+        .then(dish => dish[0] === undefined ? 
+            res.status(400).json({error: "Please enter a valid id"}) : 
+            res.status(200).json(dish))
+    .catch(err => res.status(500).json({error: err}))
+});
+
+server.post('/api/dishes', (req, res) => {
+    const changes = req.body;
+
+    if (changes.name === "" || changes.name === undefined) {
+        return res.status(400).json({error: "Please make sure the dish name is indexed."})
+    }
+
+    db.addDish(changes)
+        .then(id => res.status(200).json(id))
+        .catch(err => res.status(500).json({error: err}))
+});
+
+server.get('/api/recipes', (req, res) => {
+    db.getRecipes()
+        .then(recipes => res.status(200).json(recipes))
+        .catch(err => res.status(500).json({error: err}))
+});
+
+server.post('/api/recipes', (req, res) => {
+    const changes = req.body;
+
+    if (changes.name === "" || changes.name === undefined) {
+        return res.status(400).json({error: "Please make sure the recipe name is indexed."})
+    }
+
+    db.addRecipe(changes)
+        .then(id => res.status(200).json(id))
+        .catch(err => res.status(500).json({error: err}))
+});
 
 const port = 9001;
 
