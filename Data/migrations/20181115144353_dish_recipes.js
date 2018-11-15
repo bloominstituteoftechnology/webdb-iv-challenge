c21841949f02b244d2d70a055ff8f1690f213157
AM_Myrick <amyrick.820@gmail.com>
2018-11-15 15:05:47 -0500

    creates dish-recipes migration

--- Data/migrations/20181115144353_dish_recipes.js
+++ Data/migrations/20181115144353_dish_recipes.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+	return knex.schema.createTable('dishes_recipes', function(table) {
+		table.increments()
+		table.integer('dish_id').notNullable().unsigned().references('id').inTable('dishes')
+ 		table.integer('recipe_id').notNullable().unsigned().references('id').inTable('recipes')
+	})
+}
+ exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('dishes_recipes')
+}
