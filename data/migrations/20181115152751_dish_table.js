c465434a479eadadf7e4fb3505dd31b2cc582e0f
Daniel Ferrer <danfrrr13@gmail.com>
2018-11-15 15:29:59 -0500

    created the dish table migration

---
+++ data/migrations/20181115152751_dish_table.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('dish', tbl => {
+        tbl.increments();
+        tbl.string('name', 128);
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('dish');
+};
