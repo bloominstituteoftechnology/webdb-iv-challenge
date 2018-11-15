c08149581490a6b326c91952a0675193295732d3
bummings <eabummings@gmail.com>
2018-11-15 18:08:25 -0500

    re-did recipes table, added dishes table with primary key

---
+++ data/migrations/20181115180438_recipes_table.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("recipes", tbl => {
+    tbl.increments();
+    tbl.string("name", 255);
+    tbl
+      .integer("dishes_id")
+      .unsigned()
+      .references("id")
+      .inTable("dishes");
+  });
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("recipes");
+};
---
+++ data/migrations/20181115180658_dishes_table.js
@@ -0,0 +1,9 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("dishes", tbl => {
+    tbl.increments();
+    tbl.string("name", 255);
+  });
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("dishes");
+};
