cc2e1c38e2f5da18088366d71c8ed0bf318b0d2a
Hargobind Atwal <ethaipath@gmail.com>
2018-11-15 15:46:20 -0800

    Set up many to many relation ship

--- data/migrations/20181115124006_recipes.js
+++ data/migrations/20181115124006_recipes.js
@@ -1,41 +1,5 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('recipes', function(table) {
-    table
-      .increments('id')
-      .primary()
-      .notNullable();
-    
-    table
-      .integer('dish_id');
-
-    table
-      .integer('ingredient_id');
-
-    table
-      .string('name', 256)
-      .notNullable();
-
-    table
-      .integer('time_min')
-
-    table
-      .string('steps', 10240)
-
-    
-
-  }).then(function() {
-  return knex.schema.createTable('dishes', function(table) {
-    table
-      .increments('id')
-      .primary()
-      .notNullable();
-    
-    table
-      .string('name', 256)
-      .notNullable();
-
-  }).then(function() {
   return knex.schema.createTable('ingredients', function(table) {
     table
       .increments('id')
@@ -46,14 +10,52 @@ exports.up = function(knex, Promise) {
       .string('name', 256)
       .notNullable();
 
-  })
+  }).then(function() {
+    return knex.schema.createTable('dishes', function(table) {
+      table
+        .increments('id')
+        .primary()
+        .notNullable();
+      
+      table
+        .string('name', 256)
+        .notNullable();
 
-  })
+    }).then(function() {
+      return knex.schema.createTable('recipes', function(table) {
+          table
+            .increments('id')
+            .primary()
+            .notNullable();
+          
+          table.integer('dish_id').unsigned();
+          table
+            .foreign('dish_id')
+            .references('id')
+            .inTable('dishes');
 
+          table.integer('ingredient_id').unsigned();
+          table
+            .foreign('ingredient_id')
+            .references('id')
+            .inTable('ingredients');
+
+          table
+            .string('name', 256)
+            .notNullable();
+
+          table
+            .integer('time_min')
+
+          table
+            .string('steps', 10240)    
+      })
+    })
   })
 };
 
 exports.down = function(knex, Promise) {
   knex.deleteTableIfExists('recipes');
-  
+  knex.deleteTableIfExists('ingredients');
+  knex.deleteTableIfExists('dishes');  
 };
