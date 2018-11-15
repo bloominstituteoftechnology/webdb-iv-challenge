93c95aa8eec0b1caec0d122bf7bcd475cf346fd2
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-15 14:10:38 -0700

    migrations complete for dishes, ingredients, and instructions

--- db/migrations/20181115140102_add_dishes_table.js
+++ db/migrations/20181115140102_add_dishes_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('dishes', function(table) {
+      table.increments()
+      table.string('name',128).notNullable()
+      table.unique('name')
+
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('dishes')
 };
