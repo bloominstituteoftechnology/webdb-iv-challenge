7c20d8ffb6aa256c1f804e40f867606597231f7b
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-15 16:06:16 -0800

    ingredients for recipe junction

--- data/migrations/20181115143609_ingredients.js
+++ data/migrations/20181115143609_ingredients.js
@@ -2,11 +2,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("ingredients", tbl => {
     tbl.increments();
     tbl.string("name").notNullable();
-    tbl
-      .integer("ingredient_id")
-      .unsigned()
-      .references("id")
-      .inTable("recipes");
     tbl.float("quantity");
   });
 };
