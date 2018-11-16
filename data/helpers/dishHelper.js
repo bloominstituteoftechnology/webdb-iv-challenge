bd0ec98ad0165c84eaef70ce93574ce9e95a48fc
bummings <eabummings@gmail.com>
2018-11-16 00:26:25 -0500

    added data access files for dishes and recipes

---
+++ data/helpers/dishHelper.js
@@ -0,0 +1,19 @@
+const knex = require("knex");
+const knexConfig = require("../../knexfile");
+const db = knex(knexConfig.development);
+module.exports = {
+  getDishes,
+  addDishes,
+  getDish
+};
+function getDishes() {
+  return db("dishes");
+}
+function addDishes(dish) {
+  return db("dishes")
+    .insert(dish)
+    .then(ids => ({ id: ids[0] }));
+}
+function getDish(id) {
+  return db("dishes").where({ id: Number(id) });
+}
---
+++ data/helpers/recipeHelper.js
@@ -0,0 +1,15 @@
+const knex = require("knex");
+const knexConfig = require("../../knexfile");
+const db = knex(knexConfig.development);
+module.exports = {
+  getRecipes,
+  addRecipe
+};
+function getRecipes() {
+  return db("recipies");
+}
+function addRecipe(recipe) {
+  return db("recipe")
+    .insert(recipe)
+    .then(ids => ({ id: ids[0] }));
+}
