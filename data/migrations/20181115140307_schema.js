614fc8dacc648966a4303a386fe025b1da25d122
Brad Mortensen <b.mort86@gmail.com>
2018-11-15 17:31:20 -0700

    Final commit

--- data/migrations/20181115140307_schema.js
+++ data/migrations/20181115140307_schema.js
@@ -1,31 +1,23 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema
-  .createTable('dishes', (dishes) => {
-      dishes.increments('id').primary();
-      dishes.text('name').unique().notNull();
-  })
-  .createTable('recipes', (recipes) => {
-      recipes.increments('id').primary();
-      recipes.text('name').notNull();
-      recipes.text('dishId').references('dishes.id')
-  })
-  .createTable('ingredients', (ingredients) => {
-      ingredients.increments('id').primary();
-      ingredients.text('name').notNull();
-      ingredients.integer('recipeId').references('recipes.id');
-      ingredients.integer('quantity').notNull();
-      ingredients.text('unit').defaultTo('unit');
-  })
-  .createTable('steps', (steps) => {
-      Promise.all([
-          steps.integer('stepNumber'),
-          steps.integer('recipeId').references('recipes.id'),
-      ]).then(() => {
-          steps.primary(['stepNumber, recipeId'])
-      });
-      steps.text('description')
-  })
+    return knex.schema
+        .createTable('dishes', (dishes) => {
+        dishes.increments('id').primary();
+        dishes.text('name').unique().notNull();
+    })
+      .createTable('recipes', (recipes) => {
+        recipes.increments('id').primary();
+        recipes.text('name').notNull();
+        recipes.text('dishId').references('dishes.id')
+    })
+      .createTable('ingredients', (ingredients) => {
+        ingredients.increments('id').primary();
+        ingredients.text('name').notNull();
+        ingredients.integer('recipeId').references('recipes.id');
+        ingredients.integer('quantity').notNull();
+        ingredients.text('unit').defaultTo('unit');
+    })
+
 };
 
 exports.down = function(knex, Promise) {
@@ -34,5 +26,5 @@ exports.down = function(knex, Promise) {
         knex.schema.dropTableIfExists('steps'),
         knex.schema.dropTableIfExists('recipes'),
         knex.schema.dropTableIfExists('dishes'),
-      ]);
+    ]);
 };
