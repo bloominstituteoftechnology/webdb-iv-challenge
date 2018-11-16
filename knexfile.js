cad7372c77109556f4b781ccfa6117c96872c663
Lajawanti <lajawanti.dhake@gmail.com>
2018-11-15 22:57:54 -0500

    remove unwanted files

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,23 @@
 // Update with your config settings.
 
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
+		client: "sqlite3",
+		connection: {
+			filename: "./data/lambda.sqlite3"
+		},
+    
+    useNullAsDefault: true,
+    
+    migrations: {
+			directory: "./data/migrations"
+    },
+    
+    seeds: {
+      directory: './data/seeds'
     }
-  },
-
-  useNullAsDefault: true,
-
-  migrations: {
-      directory: './data/migrations'
-  },
-
-  seeds : {
-      directory : './data/seeds'
   }
-
+  
 };
