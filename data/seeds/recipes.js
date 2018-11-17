309f6adfe30d2d6837ad3e1651d3cb17afac40f8
Gannon <gkleblanc@gmail.com>
2018-11-16 21:11:56 -0500

    finished MVP

--- data/seeds/recipes.js
+++ data/seeds/recipes.js
@@ -5,12 +5,12 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('recipes').insert([
-        { name: 'Veggie Pizza', dishId: 1 },
-        { name: 'Meat Pizza', dishId: 1 },
-        { name: 'Veggie Soup', dishId: 2 },
-        { name: 'Meat Soup', dishId: 2 },
-        { name: 'Veggie Burger', dishId: 3 },
-        { name: 'Meat Burger', dishId: 3 }
+        { name: 'Veggie Pizza', dishId: 1 }, //1
+        { name: 'Meat Pizza', dishId: 1 },   //2
+        { name: 'Veggie Soup', dishId: 2 },  //3
+        { name: 'Meat Soup', dishId: 2 },    //4
+        { name: 'Veggie Burger', dishId: 3 }, //5
+        { name: 'Meat Burger', dishId: 3 }  //6
       ]);
     });
 };
