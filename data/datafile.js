4c84d7b29295ed825c49f489b57a2befacacae3c
Wanda <doubleuevans@gmail.com>
2018-11-29 16:01:56 -0600

    up to addDish, which is erroring

--- data/datafile.js
+++ data/datafile.js
@@ -5,9 +5,23 @@ const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
 module.exports = {
-  getDishes
-}
+  getDishes,
+  getDish,
+  addDish,
+};
 
 function getDishes() {
   return db('dishes')
-}l
+}
+
+function getDish(id) {
+  return db('dishes')
+        .where({ id: Number(id) });
+}
+
+function addDish(dish) {
+  return db('dishes')
+        .insert(dish)
+        .then(ids => ({ id: ids[0] }))
+}
+
--- index.js
+++ index.js
@@ -16,16 +16,30 @@ server.get('/api/dishes', (req, res) => {
 });
 
 //addDish(dish): should add the dish to the database and return the id of the new dish.
-server.get('/api/cohorts/:cohortid', (req, res) => {
-  const { cohortid } = req.params;
+server.get('/api/dishes/:dishid', (req, res) => {
+  const { dishid } = req.params;
   
-  db('cohorts')
-  .where({id: cohortid})
-  .then(cohorts => res.status(200).json(cohorts))
+  db.getDish(dishid)
+  .where({id: dishid})
+  .then(dishes => res.status(200).json(dishes))
   .catch(err => res.status(500).json(err));
 });
 
 //getDish(id): should return the dish with the provided id and include a list of the related recipes.
+server.post('/api/dishes', (req, res) => {
+  const dish = req.body;
+  
+  db.addDish(dish)
+    //.insert(dish)
+    //.returning('id')
+    .then(dish => {
+      res.status(201).json(dish);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error inserting dish', err });
+    });
+});
+
 //getRecipes(): should return a list of all recipes in the database including the dish they belong to.
 //addRecipe(recipe): should add a recipe to the database and return the id of the new recipe
 
