b7c7d4acfc7f0c106b2ba7ac5b0db4a0da39698c
Angelo Deleon <rudy209@comcast.net>
2018-11-29 14:27:50 -0800

    Added seeds and pizza

--- index.js
+++ index.js
@@ -8,3 +8,33 @@ server.use(express.json());
 server.get('/', async(req, res) => {
     await res.send('Hello there.');
 });
+
+server.get('/dishes', async(req, res) => {
+    try {
+        const dishes = await db.getDishes();
+        res.status(200).json(dishes);
+    } catch (error) {
+        res.status(500).json({ error: 'There was an error getting the dishes.' });
+    }
+});
+
+server.get('/dishes/:id', async(req, res) => {
+    const { id } = req.params;
+    try {
+        const dish = await db.getDish(id);
+        res.status(200).json(dish);
+    } catch (error) {
+        res.status(500).json({ error: 'There was an error getting that dish.' });
+    }
+});
+
+server.post('/dishes', async(req, res) => {
+    const newDishData = req.body;
+
+    try {
+        const dishId = await db.addDish(newDishData);
+        res.status(201).json(dishId);
+    } catch (error) {
+        res.status(500).json({ error: 'There was an error adding that dish.' });
+    }
+});
