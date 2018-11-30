ffa80f20f729fd811e3a17f3033c231711a03609
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-29 20:00:52 -0500

    added methods and applied them in index.js

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const knex = require("knex");
 
-const knexConfig = require("./knexfile.js");
 
-const db = knex(knexConfig.development);
+const db = require('./data/helpers/dataAccess.js');
 
 const server = express();
 
 server.use(express.json());
 
-
-
-
-
-
-
+db.getDishes()
+    .then(res => {
+        console.log('dishes')
+    })
+db.addDish({ name: 'fudge'})
+    .then(res => {
+        console.log('fudge')
+    })
+db.getDish(1);
+db.getRecipes();
+db.addRecipe({ name: "fudge", dish_id: "1"});
 
 
 
