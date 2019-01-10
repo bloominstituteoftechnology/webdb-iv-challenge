6698f3833612449796d6bd7387a51bcc5af8c431
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-09 23:30:51 -0500

    main setup is done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
