d5e96dc676a7f22b9354a26587013bc9a3d04119
Robert Billington <chillingtonbillington@gmail.com>
2019-01-10 10:17:23 -0600

    Shopping list migration complete

---
+++ data/migrations/20190110100917_shopping_list.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('shopping_list', table =>{
+      table.increments('id');
+      table.integer('dish_id').unsigned();
+      table.foreign('dish_id').references('id').on('dish');
+      table.string('dish_name').unsigned(); //Does unsigned need to be included for string references?
+      table.foreign('dish_name').references('dish_name').on('dish');
+      table.integer('recipe_id').unsigned().unique();
+      table.foreign('recipe_id').references('id').on('recipe');
+      table.string('recipe_name').unsigned().unique();
+      table.foreign('recipe_name').refernces('recipe_name').on('recipe');
+  })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('shopping_list');
+};
