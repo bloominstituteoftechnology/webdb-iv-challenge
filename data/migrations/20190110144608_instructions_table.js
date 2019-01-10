94fd970ff954183a6413609cdf11207a246e61db
Ben <bennybooftw@gmail.com>
2019-01-10 14:53:49 -0500

    Created instructions migration

---
+++ data/migrations/20190110144608_instructions_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  knex.schema.createTable("instructions", table => {
+    table.increments();
+    table.string("name", 280).notNullable();
+    table.integer("step");
+    table
+        .integer("recipes_id")
+        .unsigned()
+        .references("id")
+        .inTable("recipes");
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("instructions");
+};
