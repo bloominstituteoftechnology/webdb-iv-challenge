d61779a362f6ee902ab1773a4b0e8f74b53a0861
alasalle <angela.lasalle91@gmail.com>
2019-01-10 20:48:43 -0500

    initialized project set up/organized project files and folders began migrations

---
+++ data/migrations/20190110201321_recipes.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("recipes", table => {
+    table.increments("id");
+    table.text("recipe_name").notNullable();
+    table.integer("dish_id").unsigned();
+    table.foreign("dish_id").references("id").on("dishes");
+  })
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTableIfExists("recipes");
+};
