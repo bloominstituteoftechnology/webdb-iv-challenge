ff579d75d23294ee020ff7194c1f2176264667c5
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-10 22:11:02 -0500

    made recipe schema

--- data/migrations/20190109233249_recipe.js
+++ data/migrations/20190109233249_recipe.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipe', table => {
+    table.increments();
+    table.text('name').notNullable();
+    table.integer('dish_id').unsigned().references('id').inTable('dish');
+    table.integer('steps_id').unsigned().references('id').inTable('steps');
+    table.json('ingredients');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('recipe');
 };
