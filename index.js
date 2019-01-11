394a237e086ba5dd5a7d940124ff07f6e9004781
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-01-11 01:10:58 -0600

    knexfile added index.js: server and port running knexfile.js: SQLite3 added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const server = express();
 
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
+
 const PORT = process.env.PORT || 3300;
 
 server.use(express.json());
