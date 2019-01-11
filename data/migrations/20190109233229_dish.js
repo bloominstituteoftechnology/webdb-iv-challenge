363fa9dcd7174946db1f36ec51d1ad1ffa9fbaf7
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-10 22:05:02 -0500

    created steps table

--- data/migrations/20190109233229_dish.js
+++ data/migrations/20190109233229_dish.js
@@ -7,5 +7,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('cohorts');
+    return knex.schema.dropTableIfExists('dish');
 };
