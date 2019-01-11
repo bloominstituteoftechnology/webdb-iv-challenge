b94dcb46278697aa576bcd08c94a01348534832c
cferrell2 <cferrell12182@gmail.com>
2019-01-10 22:45:27 -0500

    create migration for recipes table

---
+++ data/migrations/20190110223926_recipes.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipes', function(tbl){
+      tbl.increments();
+      tbl.string('recipe_name', 255).notNullable().Unique();
+      tbl.string('prep_time', 255).notNullable();
+      tbl.string('cook_time', 255).notNullable();
+      tbl.integer('dish_id')
+        .unsigned()
+        .references('id')
+        .inTable('dishes');
+
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('recipes');
+  };
