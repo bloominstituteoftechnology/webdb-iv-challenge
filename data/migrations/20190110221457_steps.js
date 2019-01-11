a46d7b398ea8cc8deaa175e8bed42e97378467ba
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-10 23:49:36 -0500

    changed name of integer

--- data/migrations/20190110221457_steps.js
+++ data/migrations/20190110221457_steps.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('steps', table => {
     table.increments();
-    table.integer('recipe_order').notNullable();
+    table.integer('instruction_order').notNullable();
     table.string('instructions').notNullable();
     table.integer('recipe_id').unsigned().unique();
     table.foreign('recipe_id').references('id').on('recipe');
