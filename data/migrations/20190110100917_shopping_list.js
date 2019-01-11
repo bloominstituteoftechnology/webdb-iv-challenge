34a0e550bf0b3ddf78a8d839aadda57dd53a1601
Robert Billington <chillingtonbillington@gmail.com>
2019-01-10 22:06:41 -0600

    Completed db.js with helper functions for MVP goals

--- data/migrations/20190110100917_shopping_list.js
+++ data/migrations/20190110100917_shopping_list.js
@@ -10,6 +10,12 @@ exports.up = function(knex, Promise) {
       table.foreign('recipe_id').references('id').on('recipe');
       table.string('recipe_name').unsigned().unique();
       table.foreign('recipe_name').references('recipe_name').on('recipe');
+      table.string('ingredient').unsigned();
+      table.foreign('ingredient').references('ingredient').on('ingredients');
+      table.integer('quantity_to_buy');
+      table.foreign('quantity_to_buy').references('quantity_of_measurement').on('ingredients')
+      table.string('measurement').unsigned();
+      table.foreign('measurement').references('measurement_unit').on('ingredients');
   })
 };
 
