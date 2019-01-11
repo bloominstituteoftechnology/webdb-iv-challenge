7b3b6a122e44456b511952a0a0403970d94898ca
vy3191 <venkyafhs@gmail.com>
2019-01-10 23:13:25 -0500

    Project set up completed

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const knex = require('knex');
+const server = express();
+const PORT  = 5200;
+
+//Knex 
+const dbConfig = require('./knexfile.js')
+const db = knex(dbConfig.development);
+
+// middleware
+server.use(express.json());
+
+server.get('/api', (req,res) => {
+    res.json({Message: `This is app up and running now`})
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running at localhost://${PORT}`);
+})
+
