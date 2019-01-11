dca7af66c3ed8058cca635a265d853b4e6343fb7
Joe Mercado <jryel1970@gmail.com>
2019-01-10 21:20:44 -0500

    added index.js, knexfile.js and db.js

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+module.exports = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: 'data/recipe-book.db'
+		}
+	},
+
+	staging: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+
+		pool: {
+			min: 2,
+			max: 10
+		},
+
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
+
+	production: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+
+		pool: {
+			min: 2,
+			max: 10
+		},
+
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	}
+};
