c2c01be477a101c822ba1e29e6a980a1222601af
Kyle Baker <k@gnomiq.com>
2019-01-10 21:03:24 -0800

    changes to schema

--- migrations/20190110194115_ingredients.js
+++ migrations/20190110194115_ingredients.js
@@ -2,8 +2,6 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('ingredients', function (table) {
         table.increments('id');
         table.string('name').notNullable();
-        table.float('qty');
-        table.enu('uom', ['g', 'tbsp', 'tsp', 'cup', 'oz', 'fl oz', 'gal', 'l', 'lb']);
     });
 };
 
