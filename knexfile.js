1339f7a21ff029fbf8f3e65eb74855b2169318c6
Rob Salzberg <robsalzberg@gmail.com>
2019-01-10 22:40:44 -0500

    Added migrations shells

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: '.data/recipes.sqlite3'
+      filename: './data/recipes.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
-      directory: '.data/migrations'
+      directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
