d29da35394d61713c1fb4b123f13bf9b0cef87ef
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-11 17:39:37 -0700

    Added endpoint to get dishes

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/dishes', (req, res) => {
+    db.getDishes()
+        .then(dishes => {
+            res
+                .json(dishes);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({message: 'The dishes could not be retrieved at this time.'})
+        });
+});
+
 server.listen(PORT, err => {
     console.log(`Server is running on ${PORT}`);
 });
