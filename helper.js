a87794d46ae2a7491af6ec63e690e4ff242ed824
Tommmy Collison <tommy@collison.ie>
2019-01-23 19:29:47 -0800

    helper file skeleton

---
+++ helper.js
@@ -0,0 +1,21 @@
+const knex = require('knex');
+const db_config = require('./knexfile');
+const db = knex(db_config.development);
+
+// get dishes
+
+
+
+// add dish
+
+
+
+// get dish by id
+
+
+
+// get recipe
+
+
+
+// add recipe 
