52fdb5d06b6395e6780528c06cbc415890f003cd
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-24 13:49:00 -0500

    Fleshed out migrations, generated seeds, will begin adding seed data.

--- data/migrations/20190122222749_ingredient.js
+++ data/migrations/20190122222749_ingredient.js
@@ -8,10 +8,11 @@ exports.up = function(knex, Promise) {
        .integer('recipe_id')
        .unsigned()
        .references('id')
+       .inTable('recipes')
 
  })
 };
 
 exports.down = function(knex, Promise) {
-  
+ return knex.schema.dropTableIfExists('ingredients')
 };
