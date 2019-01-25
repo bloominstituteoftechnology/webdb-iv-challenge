7166990d97027645c252b9929e30c7c60d2dd8f5
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-24 19:36:31 -0500

    Added migrations so each table would have necessary foreign keys.

--- data/seeds/01_ingredients.js
+++ data/seeds/01_ingredients.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('ingredients').insert([
-        {id: 1, ingredient_name: 'Tomato Paste'},
-        {id: 2, ingredient_name: 'Sharp Cheddar'},
-        {id: 3, ingredient_name: 'Angus Beef'}
+        {id: 1, recipe_id: 1, ingredient_name: 'Tomato Paste'},
+        {id: 2, recipe_id: 2, ingredient_name: 'Sharp Cheddar'},
+        {id: 3, recipe_id: 3, ingredient_name: 'Angus Beef'}
       ]);
     });
 };
