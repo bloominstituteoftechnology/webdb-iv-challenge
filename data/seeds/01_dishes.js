7d3530af6d7cc569af452181213bb1053ecd0c22
cocoitali <cmburatto@gmail.com>
2019-01-26 07:51:35 -0800

    MVP : added seed data for dishes, tested on db browser

--- data/seeds/01_dishes.js
+++ data/seeds/01_dishes.js
@@ -1,13 +1,18 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('dishes').truncate()
+  //use truncate instead of delete, resets the increments
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('dishes').insert([
+        {dish_name: 'Ribollita'},
+        {dish_name:'Arancini'},
+        {dish_name: 'Lasagne'},
+        {dish_name: 'Gelato'},
+        {dish_name: 'Ciceri e Tria'},
+        {dish_name: 'Pasta all\'Amatriciana '},
+        {dish_name: 'Tiramisu'},
       ]);
     });
 };
