1f72daeec0fd2e16009a5c47415c7f23502dcc94
leianne louis <leilouis112@gmail.com>
2019-02-21 16:31:55 -0500

    completed recipe_ingredients tble

--- migrations/20190221162456_recipe_ingredients_tbl.js
+++ migrations/20190221162456_recipe_ingredients_tbl.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipe_ingredients', function(tbl){
+      tbl.increments('recipe_ingredient_id');
+      tbl.int('ingredient_id').unsigned();
+      tbl.foreign('ingredient_id').references('ingredients.ingredient_id')
+      tbl.int('recipe_id').unsigned();
+      tbl.foreign('recipe_id').references('recipes.recipe_id')
+      tbl.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('recipe_ingredients')
 };
