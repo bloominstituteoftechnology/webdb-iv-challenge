6623a15e6a0cf914da4f65221a96392c8d2464cc
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-02-21 17:26:04 -0500

    GET dish by id endpoint complete

--- dishes/dishesRoutes.js
+++ dishes/dishesRoutes.js
@@ -16,4 +16,19 @@ router.get("/", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+//get a dish by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  dishes
+    .getDish(id)
+    .then(dish => {
+      if (!dish) {
+        res.status(404).json({ message: "No dish found" });
+      } else {
+        res.status(200).json(dish);
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
