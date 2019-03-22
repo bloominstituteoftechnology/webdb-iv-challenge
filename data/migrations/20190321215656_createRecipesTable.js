7e8ef9734e59ad30f955655e399dcd73e03f6746
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-21 22:02:26 -0500

    Created createRecipesTable migration

---
+++ data/migrations/20190321215656_createRecipesTable.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipes', tbl => {
+    tbl.increments();
+    tbl.string('recipe_name')
+       .notNullable()
+       .unique();
+
+    tbl.integer('dish_id')
+       .unique()
+       .unsigned()
+       .reference('id')
+       .inTable('dishes')
+       .onDelete('CASCADE')
+       .onUpdate('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('recipes');
+};
