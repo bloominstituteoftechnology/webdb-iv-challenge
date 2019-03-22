a4bfef1a3e8305481cd78459ee979a9c23370ec1
Ken Barlow <kenbrlow@gmail.com>
2019-03-21 20:41:13 -0700

    Build the recipie ingredients table migrations

--- data/migrations/20190321195212_recipe_ingredients_table.js
+++ data/migrations/20190321195212_recipe_ingredients_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipe_ingredients', tbl => {
+      tbl.increments();
+      tbl.integer('recipe_id'.references('id').inTable('recipies'))
+      tbl.integer('ingredient_id'.references('id').inTable('ingredients'));
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('recipe_ingredients');
 };
