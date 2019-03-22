e71169d632b1135bead9e66c80a964c2230c3c35
120356aa <yurlovandrew@gmail.com>
2019-03-21 17:32:06 -0700

    created dishes table

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const db = require('./data/dbHelpers.js');
+const express = require('express');
+const knex = require('knex');
+const helmet = require('helmet');
+const server = express();
+
+const db = require('./data/dbHelpers.js');;
+const PORT = 9090;
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => res.send('<h2>API</h2>'));
+
+server.listen(PORT, _ => console.log('Listening on port ' + PORT));
