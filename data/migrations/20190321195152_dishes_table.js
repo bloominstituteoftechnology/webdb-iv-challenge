9b2544f3cc409cf0fd9b44bf365edff39622cfdf
Ken Barlow <kenbrlow@gmail.com>
2019-03-21 20:01:52 -0700

    Built out the recipes table

--- data/migrations/20190321195152_dishes_table.js
+++ data/migrations/20190321195152_dishes_table.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('dishes', tbl => {
     tbl.increments();
     tbl.string('name').notNullable();
-  })
+  });
 };
 
 exports.down = function(knex, Promise) {
--- data/migrations/20190321195159_recipes_table.js
+++ data/migrations/20190321195159_recipes_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.string('name').notNullable();
+      tbl.integer('dish_id').references('id').inTable('dishes');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('recipes');
 };
