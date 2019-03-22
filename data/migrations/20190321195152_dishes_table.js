0a25dbdb9e8e64ab713a9869e9cc4d70a427af7a
Ken Barlow <kenbrlow@gmail.com>
2019-03-21 19:57:00 -0700

    Built out the dishes table.

--- data/migrations/20190321195152_dishes_table.js
+++ data/migrations/20190321195152_dishes_table.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('dishes', tbl => {
+    tbl.increments();
+    tbl.string('name').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('dishes');
 };
