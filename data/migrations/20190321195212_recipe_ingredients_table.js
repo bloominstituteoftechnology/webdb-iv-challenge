479e93956ce0694649c35c77525e7cfc588d7f5e
Ken Barlow <kenbrlow@gmail.com>
2019-03-21 20:51:04 -0700

    Fixed typo on recipe ingredients table and generated the sqlite3 db

--- data/migrations/20190321195212_recipe_ingredients_table.js
+++ data/migrations/20190321195212_recipe_ingredients_table.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('recipe_ingredients', tbl => {
       tbl.increments();
-      tbl.integer('recipe_id'.references('id').inTable('recipies'))
-      tbl.integer('ingredient_id'.references('id').inTable('ingredients'));
+      tbl.integer('recipe_id').references('id').inTable('recipies');
+      tbl.integer('ingredient_id').references('id').inTable('ingredients');
   });
 };
 
