5eda18fff63c8cc9848014017faa86c0ae2ef9c4
Ken Barlow <kenbrlow@gmail.com>
2019-03-21 20:46:23 -0700

    Built the recipe instructions table migration

--- data/migrations/20190321195216_recipe_instructions_table.js
+++ data/migrations/20190321195216_recipe_instructions_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipe_instructions', tbl => {
+      tbl.increments();
+      tbl.string('instructions').notNullable();
+      tbl.integer('recipe_id').notNullable().references('id').inTable('recipes');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('recipe_instructions');
 };
