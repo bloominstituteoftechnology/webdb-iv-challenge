40839f708b6084261273391034963f1ad86e5b49
120356aa <yurlovandrew@gmail.com>
2019-03-21 18:28:59 -0700

    created helpers and tested endpoints

--- index.js
+++ index.js
@@ -11,4 +11,19 @@ server.use(express.json());
 
 server.get('/', (req, res) => res.send('<h2>API</h2>'));
 
+// GET DISHES
+db.getDishes()
+  .then(dishes => console.log('dishes', dishes))
+  .catch(err => console.log('error', err));
+
+// GET DISH BY ID
+db.getDish(1)
+  .then(dish => console.log('dish', dish))
+  .catch(err => console.log('error', err));
+
+// GET RECIPES
+db.getRecipes()
+  .then(recipes => console.log(recipes))
+  .catch(err => console.log('error', err));
+
 server.listen(PORT, _ => console.log('Listening on port ' + PORT));
