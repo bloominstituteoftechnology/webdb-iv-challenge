e26f94b44c1d738184b3041237042d346964a0cb
Ken Barlow <kenbrlow@gmail.com>
2019-03-23 20:04:04 -0700

    Setup the index.js to return the table contents when yarn server is ran while nodemon is running.

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 const db = require('./data/dbHelpers.js');
 
+db.findDishes()
+.then(dishes => {
+    console.log('dishes', dishes);
+}).catch(err => {
+    console.log('issue with dishes', err);
+});
+
+db.findRecipes()
+.then(recipes => {
+    console.log('recipes', recipes);
+}).catch(err => {
+    console.log('issue with recipes', err);
+});
+
+db.findIngredients()
+.then(ingredients => {
+    console.log('ingredients', ingredients);
+}).catch(err => {
+    console.log('issue with ingredients', err);
+});
+
+db.findRecipeIngredients()
+.then(ri => {
+    console.log('recipe_ingredients', ri);
+}).catch(err => {
+    console.log('issue with recipe_ingredients', err);
+});
+
+db.findRecipeInstructions()
+.then(ri => {
+    console.log('recipe_instructions', ri);
+}).catch(err => {
+    console.log('issue with recipe_instructions', err);
+});
