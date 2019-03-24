8f2f4e8a3bf54b4884a39429a4f880a496ee3a9a
Ken Barlow <kenbrlow@gmail.com>
2019-03-23 19:51:28 -0700

    Setup the foreign keys to come after the instructions on the recipe_instructions table.

--- data/seeds/05-recipe_instructions.js
+++ data/seeds/05-recipe_instructions.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('recipe_instructions').insert([
-        {recipe_id: 1, instructions: 'Lay the flour tortila out, put the pinto beans and chicken in.'},
-        {recipe_id: 2, instructions: 'Put the mayo on the bread and add cheese.'},
-        {recipe_id: 3, instructions: 'Flatten the pizza dough add the red sauce and pepperoni and cheese then bake.'},
+        {instructions: 'Lay the flour tortila out, put the pinto beans and chicken in.', recipe_id: 1},
+        {instructions: 'Put the mayo on the bread and add cheese.', recipe_id: 2},
+        {instructions: 'Flatten the pizza dough add the red sauce and pepperoni and cheese then bake.', recipe_id: 3},
       ]);
     });
 };
