5a6145fb825d2cfee24c4b310fa1f673ec97a73a
Ken Barlow <kenbrlow@gmail.com>
2019-03-23 19:55:38 -0700

    Setup base dbHelpers file.

---
+++ data/dbHelpers.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+    // find dishes
+    findDishes: () => {
+
+    },
+
+    // find recipes
+    findRecipes: () => {
+
+    },
+
+    // find ingredients
+    findIngredients: () => {
+
+    },
+
+    // find recipe ingredients
+    findRecipeIngredients: () => {
+
+    },
+
+    // find recipe instructions
+    findRecipeInstructions: () => {
+
+    }
+
+};
