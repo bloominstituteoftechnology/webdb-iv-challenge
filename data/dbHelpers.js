561322a8cd206391e01a90ec6ad4afaadfdaaf12
Ken Barlow <kenbrlow@gmail.com>
2019-03-23 19:59:20 -0700

    Built out db helper to return table contents.

--- data/dbHelpers.js
+++ data/dbHelpers.js
@@ -1,28 +1,32 @@
+const knex = require('knex');
+const dbConfig = require('../knexfile.js');
+const db = knex(dbConfig.development);
+
 module.exports = {
 
     // find dishes
     findDishes: () => {
-
+        return db('dishes');
     },
 
     // find recipes
     findRecipes: () => {
-
+        return db('recipes');
     },
 
     // find ingredients
     findIngredients: () => {
-
+        return db('ingredients');
     },
 
     // find recipe ingredients
     findRecipeIngredients: () => {
-
+        return db('recipe_ingredients');
     },
 
     // find recipe instructions
     findRecipeInstructions: () => {
-
+        return db('recipe_instructions');
     }
 
 };
