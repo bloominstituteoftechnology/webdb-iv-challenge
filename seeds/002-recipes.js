c5a30f434473930fa421e818770405f3e0d5a634
Kim Duclos <kimduclos@gmail.com>
2019-03-29 08:35:25 -0600

    cleaned up some old comments that didn't make sense anymore

--- seeds/002-recipes.js
+++ seeds/002-recipes.js
@@ -1,17 +1,17 @@
 
 exports.seed = function(knex, Promise) {
     return knex("recipes").insert([
-      { name: "Veggie Calzone", dish_id: 1 }, // 1
-      { name: "Cheese Calzone", dish_id: 1 }, // 2
-      { name: "Meat Calzone", dish_id: 1 }, // 3
-      { name: "Cheese Burrito", dish_id: 2 }, // 4
-      { name: "Bean Burrito", dish_id: 2 }, // 5
-      { name: "Steak Burrito", dish_id: 2 }, // 6
-      { name: "Chocolate Cake", dish_id: 3 }, // 7
-      { name: "Angel Cake", dish_id: 3 }, // 8
-      { name: "3-Layer Cake", dish_id: 3 }, // 9
-      { name: "Veggie Burger", dish_id: 4 }, // 10
-      { name: "Caesar Salad", dish_id: 4 }, // 11
-      { name: "Cobb Salad", dish_id: 4 } // 12
+      { name: "Veggie Calzone", dish_id: 1 }, 
+      { name: "Cheese Calzone", dish_id: 1 }, 
+      { name: "Meat Calzone", dish_id: 1 }, 
+      { name: "Cheese Burrito", dish_id: 2 }, 
+      { name: "Bean Burrito", dish_id: 2 }, 
+      { name: "Steak Burrito", dish_id: 2 }, 
+      { name: "Chocolate Cake", dish_id: 3 }, 
+      { name: "Angel Cake", dish_id: 3 }, 
+      { name: "3-Layer Cake", dish_id: 3 }, 
+      { name: "Veggie Burger", dish_id: 4 }, 
+      { name: "Caesar Salad", dish_id: 4 }, 
+      { name: "Cobb Salad", dish_id: 4 } 
     ]);
   };
