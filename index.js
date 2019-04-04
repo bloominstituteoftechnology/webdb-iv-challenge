d5ab72b134f4f9761af3fb7ec03891d50d18e8e9
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-04-03 21:57:23 -0400

    setup, server, imports, server.use

--- index.js
+++ index.js
@@ -11,78 +11,11 @@ const mw = require('./middleware');
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
-
 server.use(mw.errorHandler);
 
-server.get('/', (req, res) => {
-  res.send('It works mon');
-});
 
-server.get('/api/dishes', (req, res, next) => {
-  dishdb
-    .getDishes()
-    .then(data => {
-      return res.status(200).json(data);
-    })
-    .catch(err => next(err));
-});
 
-server.get('/api/dishes/:id', (req, res, next) => {
-  dishdb
-    .getDish(req.params.id)
-    .then(data => {
-      if (!data) {
-        return next({ code: 404 });
-      }
-      res.status(200).json(data);
-    })
-    .catch(err => next(err));
-});
-
-server.post('/api/dishes', (req, res, next) => {
-  let body = req.body;
-  dishdb
-    .addDish(body)
-    .then(data => res.status(200).json(data))
-    .catch(err => next(err));
-});
-
-server.get('/api/recipes', (req, res, next) => {
-  recipedb
-    .getRecipes()
-    .then(data => res.status(200).json(data))
-    .catch(err => next(err));
-});
-
-server.post('/api/recipes', (req, res, next) => {
-  let body = req.body;
-  recipedb
-    .addRecipe(body)
-    .then(data => res.status(200).json(data))
-    .catch(err => next(err));
-});
-
-server.get('/api/recipes/:id', (req, res, next) => {
-  recipedb
-    .getRecipe(req.params.id)
-    .then(data => {
-      console.log(data);
-      if (!data) {
-        return next({ code: 404 });
-      }
-      res.status(200).json(data);
-    })
-    .catch(err => next(err));
-});
-
-server.get('/api/ingredients', (req, res, next) => {
-  ingredientdb
-    .getIngredients()
-    .then(data => res.status(200).json(data))
-    .catch(err => next(err));
-});
-
-const port = 8000;
+const port = 4000;
 server.listen(process.env.PORT || port, function() {
   console.log(`\n=*= API rolling on port http://localhost:${port} mon =*=\n`);
 });
