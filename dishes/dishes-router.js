2fec4b5d16e6b5035e29b88d199b57141e41c401
Timothy Shores <timothymshores@gmail.com>
2019-05-02 18:06:33 -0500

    Create dishes/dishes-model.js, import data/dbConfig and export find(). Create dishes router, import dishes-model and export get '/'. Import dishes router into server.js to use route '/api/dishes'

---
+++ dishes/dishes-router.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const Dishes = require('./dishes-model');
+
+router.get('/', (req, res) => {
+    const message500 = { message: 'Unable to get dishes' };
+
+    Dishes.find()
+        .then(dishes => {
+            res.status(200).json(dishes);
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json(message500);
+        });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const dishesRouter = require('./dishes/dishes-router');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api/dishes', dishesRouter);
 
 // Hello World test route
 server.get('/', (req, res) => {
