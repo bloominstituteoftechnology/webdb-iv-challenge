a024b33666e77984e5193d1b42771cc8c80dc5f2
Timothy Shores <timothymshores@gmail.com>
2019-05-02 17:44:31 -0500

    Update migrations/20190502173533_recipeIngredients with the fields primary key id, foreign key recipe_id, foreign key ingredient_id  and float quantity

--- data/migrations/20190502173533_recipeIngredients.js
+++ data/migrations/20190502173533_recipeIngredients.js
@@ -1,8 +1,29 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('recipeIngredients', tbl => {
+        tbl.increments();
+        tbl
+            .integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('dishes')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        tbl
+            .integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('ingredients')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        tbl
+            .float('quantity')
+            .notNullable()
+    })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('recipeIngredients')
 };
