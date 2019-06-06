70d08142804c892bf7eb2118fc4919d7499137f6
Manju KC <Manju KC>
2019-06-06 15:58:12 -0400

    created dish table

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const server = express();
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+const db = knex(knexConfig.development);
 
 server.use(helmet());
 server.use(express.json());
