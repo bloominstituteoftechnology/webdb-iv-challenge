ce359e1a0b4fa9f9eb956654ecac838a01b17379
Manju KC <Manju KC>
2019-06-06 16:41:39 -0400

    updated some code

---
+++ migrations/20190606163109_create_ingredient_table.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipes', function(tbl) {
+
+        tbl.increments().unique()
+
+        tbl
+          .string('name', 128)
+          .notNullable() 
+          .unique()
+
+        tbl
+          .string('instructions')
+          .notNullable() 
+
+        tbl
+          .integer('dish_id') 
+          .unsigned()
+          .notNullable() 
+          .references('id')
+          .inTable('dishes') 
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE')    
+    })
+};
+
+
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('recipes')
+
+};
